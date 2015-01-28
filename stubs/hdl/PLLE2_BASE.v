573d322d16c81edace0efd095be1faf0504057fc
Fred Huettig <Fred@Adapteva.com>
2015-01-28 14:15:13 -0500

    Revert "Fixing port declarations (thanks Verilator!)"
    This reverts commit 75310447401bce8561b7a67d4c8aeca27c261464.

--- stubs/hdl/PLLE2_BASE.v
+++ stubs/hdl/PLLE2_BASE.v
@@ -1,7 +1,7 @@
 module PLLE2_BASE (/*AUTOARG*/
    // Outputs
-   CLKFBOUT, LOCKED, CLKOUT0, CLKOUT1, CLKOUT2, CLKOUT3, CLKOUT4,
-   CLKOUT5,
+   CLKFB, LOCKED, CLKOUT0, CLKOUT1, CLKOUT2, CLKOUT3, CLKOUT4,
+   CLKOUT5, CLKFBOUT,
    // Inputs
    CLKIN1, RST, PWRDWN, CLKFBIN
    );
@@ -43,8 +43,9 @@ module PLLE2_BASE (/*AUTOARG*/
    input RST;
    input PWRDWN;
    input CLKFBIN;
-
-   output CLKFBOUT;      
+   
+   
+   output CLKFB;
    output LOCKED;
    output CLKOUT0;
    output CLKOUT1;
@@ -52,7 +53,7 @@ module PLLE2_BASE (/*AUTOARG*/
    output CLKOUT3;
    output CLKOUT4;
    output CLKOUT5;
-
+   output CLKFBOUT;
    
    
 endmodule // PLLE2_BASE
