00a921b8399af1e540750e49e0bb20925a4a2c6f
Andreas Olofsson <andreas@adapteva.com>
2015-04-18 16:21:45 -0400

    Changed register map -Moved "groups" to E,D,C -Changed names to EL* (shorter is better, clear enough) -Moved order to fit logical operation during init -Moved embox registers to MMR group DONE!

--- elink/hdl/elink_regmap.v
+++ elink/hdl/elink_regmap.v
@@ -2,25 +2,22 @@
 //Group set with bits 19:16
 
 //Epiphany Register Memory Map
-`define EGROUP_MMR   4'hF
-`define EGROUP_RXMMU 4'hE
-`define EGROUP_TXMMU 4'hD
-`define EGROUP_EMBOX 4'hC
+`define EGROUP_MMR   4'hE
+`define EGROUP_RXMMU 4'hD
+`define EGROUP_TXMMU 4'hC
 
 //ELINK REGISTERS addr[6:2]
-`define ESYSRESET    5'h0
-`define ESYSTX       5'h1
-`define ESYSRX       5'h2
-`define ESYSCLK      5'h3
-`define ESYSCOREID   5'h4
-`define ESYSVERSION  5'h5
-`define ESYSDATAIN   5'h6
-`define ESYSDATAOUT  5'h7
-`define ESYSDEBUG    5'h8
-
-//MESSAGE BOX
-`define EMBOXLO      5'h0
-`define EMBOXHI      5'h1
+`define ELRESET    5'h0
+`define ELCLK      5'h1
+`define ELTX       5'h2
+`define ELRX       5'h3
+`define ELCOREID   5'h4
+`define ELVERSION  5'h5
+`define ELDATAIN   5'h6
+`define ELDATAOUT  5'h7
+`define ELDEBUG    5'h8
+`define EMBOXLO    5'h9
+`define EMBOXHI    5'hA
 
 //RX MMU addr[15:0]
 
