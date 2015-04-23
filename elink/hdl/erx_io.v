842a6d894a676be25367fdd3b8571607dadc9fbc
Andreas Olofsson <andreas@adapteva.com>
2015-04-23 18:01:19 -0400

    Fixing enable/reset: -Removing enable from ISERDES, not healthy -Moving all logic to protocol block. (this is an IO block) -Removing tow redundant pipeline stages (check this??)

--- elink/hdl/erx_io.v
+++ elink/hdl/erx_io.v
@@ -4,23 +4,20 @@ module erx_io (/*AUTOARG*/
    rx_lclk_div4, rx_frame_par, rx_data_par, ecfg_rx_datain,
    // Inputs
    reset, rxi_lclk_p, rxi_lclk_n, rxi_frame_p, rxi_frame_n,
-   rxi_data_p, rxi_data_n, rx_wr_wait, rx_rd_wait, ecfg_rx_enable,
-   ecfg_rx_gpio_enable, ecfg_dataout
+   rxi_data_p, rxi_data_n, rx_wr_wait, rx_rd_wait
    );
 
-   parameter IOSTD_ELINK = "LVDS_25";
-
+   parameter IOSTANDARD = "LVDS_25";
 
    //###########
-   //# eLink pins
+   //# reset
    //###########
-   input       reset;      // Reset (from ecfg)
-
+   input       reset;                        // Reset (from ecfg)
 
    //###########
    //# eLink pins
    //###########
-   input        rxi_lclk_p,  rxi_lclk_n;     //link rx clock input
+   input        rxi_lclk_p,   rxi_lclk_n;    //link rx clock input
    input        rxi_frame_p,  rxi_frame_n;   //link rx frame signal
    input [7:0]  rxi_data_p,   rxi_data_n;    //link rx data
    output       rxo_wr_wait_p,rxo_wr_wait_n; //link rx write pushback output
@@ -29,47 +26,39 @@ module erx_io (/*AUTOARG*/
    //#############
    //# Fabric interface, 1/8 bit rate of eLink
    //#############
-   output        rx_lclk_div4; // Parallel clock output (slow)
+   output        rx_lclk_div4;               // Parallel clock output (slow)
    output [7:0]  rx_frame_par;
    output [63:0] rx_data_par;
    input         rx_wr_wait;
    input         rx_rd_wait;
    
    //#############
-   //# Configuration bits
-   //#############
-   input         ecfg_rx_enable;         //enable signal for rx  
-   input         ecfg_rx_gpio_enable;    //forces rx wait pins to constants
-   input [1:0] 	 ecfg_dataout;           //rd_wait, wr_wait for GPIO mode
+   //# Direct sampling mode
+   //##############
    output [8:0]  ecfg_rx_datain;         //gpio data in (data in and frame)
 
    //############
-   //# REGS
-   //############
-   reg [63:0]    rx_data_par;    // output registers
-   reg [7:0]     rx_frame_par;
-   
-   //############
    //# WIRES
    //############
-   wire [7:0]    rx_data;  // High-speed serial data
-   wire          rx_frame; // serial frame
-   wire          serdes_reset;
+   wire [7:0]    rx_data;         // High-speed serial data
+   wire          rx_frame;        // serial frame
+   wire          rx_lclk;         // Single-ended clock
+   wire 	 rx_lclk_s;       // Serial clock after BUFIO
    
    //################################
    //# Input Buffers Instantiation
    //################################
    IBUFDS
-	 #(.DIFF_TERM  ("TRUE"),     // Differential termination
-       .IOSTANDARD (IOSTD_ELINK))
+	 #(.DIFF_TERM  ("TRUE"),
+       .IOSTANDARD (IOSTANDARD))
 	 ibufds_rxdata[7:0]
 	   (.I     (rxi_data_p[7:0]),
         .IB    (rxi_data_n[7:0]),
         .O     (rx_data[7:0]));
 
    IBUFDS
-	 #(.DIFF_TERM  ("TRUE"),     // Differential termination
-       .IOSTANDARD (IOSTD_ELINK))
+	 #(.DIFF_TERM  ("TRUE"), 
+       .IOSTANDARD (IOSTANDARD))
 	 ibufds_rx_frame
 	   (.I     (rxi_frame_p),
         .IB    (rxi_frame_n),
@@ -79,12 +68,9 @@ module erx_io (/*AUTOARG*/
    //# Clock Buffers
    //#####################
 
-   wire          rx_lclk;  // Single-ended clock
-   wire          rx_lclk_s;  // Serial clock after BUFIO
-   
    IBUFGDS
-     #(.DIFF_TERM  ("TRUE"),   // Differential termination
-       .IOSTANDARD (IOSTD_ELINK))
+     #(.DIFF_TERM  ("TRUE"),
+       .IOSTANDARD (IOSTANDARD))
    ibufds_rxlclk
      (.I          (rxi_lclk_p),
       .IB         (rxi_lclk_n),
@@ -106,12 +92,7 @@ module erx_io (/*AUTOARG*/
 
    //#############################
    //# Deserializer instantiations
-   //#############################
-
-   wire [63:0]   rx_data_des;
-   wire [7:0]    rx_frame_des;
-   wire          rx_lclk_sn = ~rx_lclk_s;//TODO: Not important, only for MEMORY_QDR mode?
-   
+   //#############################   
    genvar        i;
    generate for(i=0; i<8; i=i+1)
      begin : gen_serdes
@@ -142,14 +123,14 @@ module erx_io (/*AUTOARG*/
           (
            .O(),  // 1-bit output: Combinatorial output
            // Q1 - Q8: 1-bit (each) output: Registered data outputs
-           .Q1(rx_data_des[i]),      // Last data in?
-           .Q2(rx_data_des[i+8]),
-           .Q3(rx_data_des[i+16]),
-           .Q4(rx_data_des[i+24]),
-           .Q5(rx_data_des[i+32]),
-           .Q6(rx_data_des[i+40]),
-           .Q7(rx_data_des[i+48]),
-           .Q8(rx_data_des[i+56]),   // First data in?
+           .Q1(rx_data_par[i]),      // Last data in?
+           .Q2(rx_data_par[i+8]),
+           .Q3(rx_data_par[i+16]),
+           .Q4(rx_data_par[i+24]),
+           .Q5(rx_data_par[i+32]),
+           .Q6(rx_data_par[i+40]),
+           .Q7(rx_data_par[i+48]),
+           .Q8(rx_data_par[i+56]),   // First data in?
            // SHIFTOUT1, SHIFTOUT2: 1-bit (each) output: Data width expansion output ports
            .SHIFTOUT1(),
            .SHIFTOUT2(),
@@ -164,7 +145,7 @@ module erx_io (/*AUTOARG*/
            .CLKDIVP(1'b0),  // 1-bit input: TBD
            // Clocks: 1-bit (each) input: ISERDESE2 clock input ports
            .CLK(rx_lclk_s),   // 1-bit input: High-speed clock
-           .CLKB(rx_lclk_sn),     // 1-bit input: High-speed secondary clock
+           .CLKB(~rx_lclk_s),     // 1-bit input: High-speed secondary clock
            .CLKDIV(rx_lclk_div4), // 1-bit input: Divided clock
            .OCLK(1'b0),     // 1-bit input: High speed output clock used when INTERFACE_TYPE="MEMORY"
            // Dynamic Clock Inversions: 1-bit (each) input: Dynamic clock inversion pins to switch clock polarity
@@ -174,8 +155,8 @@ module erx_io (/*AUTOARG*/
            .D(rx_data[i]),      // 1-bit input: Data input
            .DDLY(1'b0),         // 1-bit input: Serial data from IDELAYE2
            .OFB(1'b0),          // 1-bit input: Data feedback from OSERDESE2
-           .OCLKB(1'b0),   // 1-bit input: High speed negative edge output clock
-           .RST(serdes_reset), // 1-bit input: Active high asynchronous reset
+           .OCLKB(1'b0),       // 1-bit input: High speed negative edge output clock
+           .RST(reset),        // 1-bit input: Active high asynchronous reset
            // SHIFTIN1, SHIFTIN2: 1-bit (each) input: Data width expansion input ports
            .SHIFTIN1(1'b0),
            .SHIFTIN2(1'b0)
@@ -210,14 +191,14 @@ module erx_io (/*AUTOARG*/
      (
       .O(),  // 1-bit output: Combinatorial output
       // Q1 - Q8: 1-bit (each) output: Registered data outputs
-      .Q1(rx_frame_des[0]),
-      .Q2(rx_frame_des[1]),
-      .Q3(rx_frame_des[2]),
-      .Q4(rx_frame_des[3]),
-      .Q5(rx_frame_des[4]),
-      .Q6(rx_frame_des[5]),
-      .Q7(rx_frame_des[6]),
-      .Q8(rx_frame_des[7]),
+      .Q1(rx_frame_par[0]),
+      .Q2(rx_frame_par[1]),
+      .Q3(rx_frame_par[2]),
+      .Q4(rx_frame_par[3]),
+      .Q5(rx_frame_par[4]),
+      .Q6(rx_frame_par[5]),
+      .Q7(rx_frame_par[6]),
+      .Q8(rx_frame_par[7]),
       // SHIFTOUT1, SHIFTOUT2: 1-bit (each) output: Data width expansion output ports
       .SHIFTOUT1(),
       .SHIFTOUT2(),
@@ -243,84 +224,36 @@ module erx_io (/*AUTOARG*/
       .DDLY(1'b0),         // 1-bit input: Serial data from IDELAYE2
       .OFB(1'b0),          // 1-bit input: Data feedback from OSERDESE2
       .OCLKB(1'b0),   // 1-bit input: High speed negative edge output clock
-      .RST(serdes_reset), // 1-bit input: Active high asynchronous reset
+      .RST(reset), // 1-bit input: Active high asynchronous reset
       // SHIFTIN1, SHIFTIN2: 1-bit (each) input: Data width expansion input ports
       .SHIFTIN1(1'b0),
       .SHIFTIN2(1'b0)
       );
 
-   // Sync control signals into our RX clock domain
-   reg [1:0]   rxenb_sync;
-   wire        rxenb = rxenb_sync[0];
-   assign      serdes_reset = ~rxenb;
-   reg [1:0]   rxgpio_sync;
-   wire        rxgpio = rxgpio_sync[0];
-   
-   // Register outputs once for good measure, then mux in loopback data if enabled
-   reg [63:0]  rx_data_reg;
-   reg [7:0]   rx_frame_reg;
-
-   
-   wire        rxreset = reset | ~ecfg_rx_enable;
-   
-   always @ (posedge rx_lclk_div4 or posedge rxreset) 
-     begin
-	if(rxreset)
-          rxenb_sync[1:0] <= 'd0;
-	else
-          rxenb_sync[1:0] <= {1'b1, rxenb_sync[1]};
-     end
-
-   //TODO: Is this the right place for the enable signal?
-   always @ (posedge rx_lclk_div4) 
-     begin      
-	rxgpio_sync       <= {ecfg_rx_gpio_enable, rxgpio_sync[1]};      
-	rx_data_reg[63:0] <= rx_data_des[63:0];
-	rx_data_par[63:0] <= rx_data_reg[63:0];
-	rx_frame_reg[7:0] <= rx_frame_des[7:0] & {8{rxenb}} & {8{~rxgpio}};     
-	rx_frame_par[7:0] <= rx_frame_reg[7:0];
-     end
-
-//#############
-//# GPIO mode inputs
-//#############
-reg [8:0] datain_reg;
-reg [8:0] ecfg_rx_datain;
-
-   always @ (posedge rx_lclk_div4) 
-     begin
-      datain_reg[8]       <= rx_frame_par[0];
-      datain_reg[7:0]     <= rx_data[7:0];
-      ecfg_rx_datain[8:0] <= datain_reg[8:0];      
-   end
-
    //#############
    //# Wait signals (asynchronous)
    //#############
 
-   wire rd_wait = rxgpio ? ecfg_dataout[0]  : rx_rd_wait;
-   wire wr_wait = rxgpio ? ecfg_dataout[1] : rx_wr_wait;
-
    OBUFDS 
      #(
-       .IOSTANDARD(IOSTD_ELINK),
+       .IOSTANDARD(IOSTANDARD),
        .SLEW("SLOW")
        ) OBUFDS_RXWRWAIT
        (
         .O(rxo_wr_wait_p),
         .OB(rxo_wr_wait_n),
-        .I(wr_wait)
+        .I(rx_wr_wait)
         );
    
    OBUFDS 
      #(
-       .IOSTANDARD(IOSTD_ELINK),
+       .IOSTANDARD(IOSTANDARD),
        .SLEW("SLOW")
        ) OBUFDS_RXRDWAIT
        (
         .O(rxo_rd_wait_p),
         .OB(rxo_rd_wait_n),
-        .I(rd_wait)
+        .I(rx_rd_wait)
         );
    
 endmodule // erx_io
