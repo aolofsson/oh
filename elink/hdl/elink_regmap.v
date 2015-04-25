919a5fa5e83efcecd05fa7aa1322d7afcb84b715
Andreas Olofsson <andreas@adapteva.com>
2015-04-25 07:09:52 -0400

    Register map twiddles -Fixed test to correspond to new map

--- elink/hdl/elink_regmap.v
+++ elink/hdl/elink_regmap.v
@@ -26,17 +26,17 @@
 
 //ELINK RX registers
 `define ELRXCFG      4'h0 //C0000-config
-`define ELRXSTATUS   4'h1 //C000C-status register
-`define ELRXBASE     4'h2 //C0004-memory base
-`define ELRXREMAP    4'h2 //C0004-memory base
-`define ELRXDIN      4'h3 //C0008-sampled data
-`define EMAILBOXLO   4'h4 //C000C-mailbox
-`define EMAILBOXHI   4'h5 //C0010-mailbox
-`define ERXRES0      4'h6 //C0014-reserved
-`define ERXRES1      4'h7 //C0018-reserved
-`define EDMACFG      4'h8 //C001C-dma
-`define EDMASTATUS   4'h9 //C0020-dma
-`define EDMASRC      4'hA //C0024-dma
-`define EDMADST      4'hB //C0028-dma
-`define EDMACOUNT    4'hC //C002C-dma 
+`define ELRXSTATUS   4'h1 //C0004-status register
+`define ELRXBASE     4'h2 //C0008-memory base
+`define ELRXREMAP    4'h2 //C000c-memory base
+`define ELRXDIN      4'h3 //C0010-sampled data
+`define EMAILBOXLO   4'h4 //C0014-mailbox
+`define EMAILBOXHI   4'h5 //C0018-mailbox
+`define EXXX0        4'h6 //C001c-reserved
+`define EXXX1        4'h7 //C0020-reserved
+`define EDMACFG      4'h8 //C0024-dma
+`define EDMASTATUS   4'h9 //C0028-dma
+`define EDMASRC      4'hA //C002C-dma
+`define EDMADST      4'hB //C0030-dma
+`define EDMACOUNT    4'hC //C0034-dma 
 
