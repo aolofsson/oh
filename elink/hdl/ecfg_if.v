4059a6eaa2960ffe1a2edf71db5d14a186c5ce4a
Andreas Olofsson <andreas@adapteva.com>
2015-05-01 17:51:12 -0400

    Created unified one clock modular confi for RX/TX -Solves clock domain crossing uglyness -Very nice and clean!! -Only compromise is that the RESET and CLOCK registers aren't readable

--- elink/hdl/ecfg_if.v
+++ elink/hdl/ecfg_if.v
@@ -1,203 +1,173 @@
 /*
  ########################################################################
- ELINK CONFIGURATION INTERFACE
+ 
  ########################################################################
  */
 
 module ecfg_if (/*AUTOARG*/
    // Outputs
-   txwr_wait, txrd_wait, mi_txcfg_en, mi_basecfg_en, mi_txmmu_en,
-   mi_rxcfg_en, mi_mailbox_en, mi_dma_en, mi_rxmmu_en, mi_we, mi_addr,
-   mi_din,
+   wait_out, mi_mmu_en, mi_dma_en, mi_cfg_en, mi_we, mi_addr, mi_din,
+   access_out, packet_out,
    // Inputs
-   sys_clk, tx_lclk_div4, rx_lclk_div4, reset, txwr_access,
-   txwr_packet, txrd_access, txrd_packet, rxwr_access, rxwr_packet,
-   mi_el_dout, mi_rx_dout, mi_tx_dout, mi_mailbox_dout
+   clk, reset, access_in, packet_in, mi_dout0, mi_dout1, mi_dout2,
+   mi_dout3, wait_in
    );
 
-   parameter ID     = 12'h800;
-   parameter DW     = 32;
-   parameter AW     = 32;
+   parameter RX     = 0;     //0,1
    parameter PW     = 104;
+   parameter AW     = 32;
+   parameter DW     = 32;
+   parameter ID     = 12'h810;
    
-
    /********************************/
    /*Clocks/reset                  */
    /********************************/  
-   input           sys_clk;
-   input           tx_lclk_div4;
-   input           rx_lclk_div4;
-   input           reset;
+   input             clk;  
+   input             reset;
 
    /********************************/
-   /*Transmit Write Interface      */
+   /*Incoming Packet               */
    /********************************/  
-   input 	   txwr_access;
-   input [PW-1:0]  txwr_packet;
-   output 	   txwr_wait;
-   /********************************/
-   /*Transmit Side Read Interface */
-   /********************************/
-   input 	   txrd_access;
-   input [PW-1:0]  txrd_packet;
-   output 	   txrd_wait;
-
-   /********************************/
-   /*Receiver Write Interface      */
-   /********************************/  
-   input 	   rxwr_access;
-   input [PW-1:0]  rxwr_packet;
+   input 	     access_in;
+   input [PW-1:0]    packet_in;
+   output 	     wait_out;     //outgoing wait
   
    /********************************/
-   /*TX Register Interface         */
+   /* Register Interface           */
    /********************************/
-   output 	   mi_tx_cfg_en;     
-   output 	   mi_tx_mmu_en;      
-   output 	   mi_tx_we;  
-   output [19:0]   mi_tx_addr;
-   output [63:0]   mi_tx_din;
+   output 	 mi_mmu_en;     
+   output 	 mi_dma_en;
+   output 	 mi_cfg_en;      
+   output        mi_we;  
+   output [14:0] mi_addr;
+   output [63:0] mi_din;   
+   input [63:0]  mi_dout0;
+   input [63:0]  mi_dout1;
+   input [63:0]  mi_dout2;
+   input [63:0]  mi_dout3;
+
 
    /********************************/
-   /*RX Register Interface         */
+   /* Outgoing Packet              */
    /********************************/
-   output 	   mi_rx_cfg_en;      
-   output 	   mi_rx_dma_en;   
-   output 	   mi_rx_mmu_en;
-   output 	   mi_rx_we;  
-   output [19:0]   mi_rx_addr;
-   output [63:0]   mi_rx_din;
-   
-   /********************************/
-   /*SYS_CLK Register Interface    */
-   /********************************/   
-   output 	   mi_basecfg_en;   
-   output 	   mi_mailbox_en;   
-   output 	   mi_we;   
-   output [19:0]   mi_addr;
-   output [63:0]   mi_din;
-   
-   /******************************/
-   /*Readback Data               */
-   /******************************/
-   input [31:0]    mi_el_dout;
-   input [DW-1:0]  mi_rx_dout;   
-   input [DW-1:0]  mi_tx_dout;
-   input [DW-1:0]  mi_mailbox_dout;
+   output 	     access_out;
+   output [PW-1:0]   packet_out;
+   input 	     wait_in;       //incoming wait 
    
    //wires
-   wire [63:0] 	   txwr_data;
-   wire [63:0] 	   rxwr_data;
+   wire [31:0] 	 dstaddr;
+   wire [31:0] 	 data;   
+   wire [31:0]   srcaddr;
+   wire [1:0] 	 datamode;
+   wire [3:0] 	 ctrlmode;
+   wire [63:0] 	 mi_dout_mux;
+   wire 	 mi_rd;
+   wire 	 access_forward;
    
-   wire [AW-1:0]   txwr_dstaddr;
-   wire [AW-1:0]   rxwr_dstaddr;   
-   wire [AW-1:0]   txrd_dstaddr;
-   wire [AW-1:0]   txrd_srcaddr;   
-
-   wire 	   mi_wr;
-   wire 	   mi_rd;
-   reg [63:0] 	   rx_mi_data_reg;
-   reg [31:0] 	   rx_mi_addr_reg;
-   reg 		   rx_mi_wait;
-
+   //regs;
+   reg 		 access_out;   
+   reg [31:0] 	 dstaddr_out;
+   reg [31:0] 	 data_out;   
+   reg [31:0] 	 srcaddr_out;
+   reg [1:0] 	 datamode_out;
+   reg [3:0] 	 ctrlmode_out;
+   reg 		 write_out;
    
-   //splicing packets
-   packet2emesh p2e_txwr(.access_out	(),
-		       .write_out	(),
-		       .datamode_out	(),
-		       .ctrlmode_out	(),
-		       .dstaddr_out	(txwr_dstaddr[AW-1:0]),
-		       .data_out	(txwr_data[31:0]),
-		       .srcaddr_out	(txwr_data[63:32]),
-		       .packet_in	(txwr_packet[PW-1:0])
+   //splicing packet
+   packet2emesh p2e (.access_out   (),
+		     .write_out	   (write),
+		     .datamode_out (datamode[1:0]),
+		     .ctrlmode_out (ctrlmode[3:0]),
+		     .dstaddr_out  (dstaddr[31:0]),
+		     .data_out	   (data[31:0]),
+		     .srcaddr_out  (srcaddr[31:0]),
+		     .packet_in	   (packet_in[PW-1:0])
 		    );
 
-   packet2emesh p2e_txrd(.access_out	(),
-		       .write_out	(),
-		       .datamode_out	(),
-		       .ctrlmode_out	(),
-		       .dstaddr_out	(txrd_dstaddr[AW-1:0]),
-		       .data_out	(),
-		       .srcaddr_out	(txrd_srcaddr[AW-1:0]),
-		       .packet_in	(txrd_packet[PW-1:0])
-		    );
+   //ENABLE SIGNALS
+   assign mi_match   = access_in & (dstaddr[31:20]==ID);
+   
+   //signal to carry transaction from ETX to ERX block through fifo_cdc
+   assign mi_rx_en = mi_match &
+                     (
+		      ((dstaddr[19:16]==4'hF) & (dstaddr[10:8]==3'h3))  |           //RX-CFG
+		      ((dstaddr[19:16]==4'hF) & (dstaddr[10:8]==3'h5) & dstaddr[5]) | //RX-DMA
+		      ((dstaddr[19:16]==4'hE) & dstaddr[15])                          //RX-EMMU
 
-   packet2emesh p2e_rxwr(.access_out	(),
-		       .write_out	(),
-		       .datamode_out	(),
-		       .ctrlmode_out	(),
-		       .dstaddr_out	(rxwr_dstaddr[AW-1:0]),
-		       .data_out	(rxwr_data[31:0]),
-		       .srcaddr_out	(rxwr_data[63:32]),
-		       .packet_in	(rxwr_packet[PW-1:0])
-		    );
-            
-   assign tx_wr = txwr_access & (txwr_dstaddr[31:20]==ID); 
-   assign tx_rd = txrd_access & (txrd_dstaddr[31:20]==ID);
-   assign rx_wr = rxwr_access & (rxwr_dstaddr[31:20]==ID); 
+		     );
+
+   //config select (group 2 and 3)
+   assign mi_cfg_en = mi_match & 
+		      (dstaddr[19:16]==4'hF) &
+		      (dstaddr[10:8]=={2'b01,RX});
+   
+
+   //dma select (group 5)
+   assign mi_dma_en = mi_match &
+		      (dstaddr[19:16]==4'hF) & 
+		      (dstaddr[10:8]==3'h5)  & 
+		      (dstaddr[5]==RX);
+   
+
+   //mmu select
+   assign mi_mmu_en = mi_match & 
+		      (dstaddr[19:16]==4'hE) &
+		      (dstaddr[15]==RX);
+
+
+   //read/write indicator
+   assign mi_rd = ~write & (mi_mmu_en | mi_cfg_en | mi_dma_en);   
+   assign mi_we = write  & (mi_mmu_en | mi_cfg_en | mi_dma_en); 
+   
+   //ADDR
+   assign mi_addr[14:0] = dstaddr[14:0];
+   
+   //DIN
+   assign mi_din[63:0]  = {srcaddr[31:0], data[31:0]};
+   
+   //READBACK MUX (inputs should be zero if not used)
+   assign mi_dout_mux[63:0] = mi_dout0[63:0] |
+			      mi_dout1[63:0] |
+			      mi_dout2[63:0] |
+			      mi_dout3[63:0];
      
-   assign mi_wr = tx_wr | rx_wr;
-   assign mi_rd = tx_rd; //no access from receiver
-    
-   //DODO: 64 bit writes?
-   assign mi_we           = mi_wr;   
-   assign mi_en           = mi_wr | mi_rd;
 
-   
-   //Enable signals (keep decoding in one place!!!)
-   
+   //Access out packet
   
- 
-   //Read/write address
-   assign mi_addr[19:0] =  rx_wr ? rxwr_dstaddr[19:0] :
-			   tx_rd ? txrd_dstaddr[19:0] :
-			           txwr_dstaddr[19:0];
+   assign access_forwad = (mi_rx_en | mi_rd) & ~wait_in;
+
+   always @ (posedge  clk)
+     access_out   <= access_forward;
    
-   //Data (prepare for it)
-   assign mi_din[63:0]  =  rx_wr ? rxwr_data[63:0] :
-                                   txwr_data[63:0];
-     
-   //Wait signals
-   assign txwr_wait = tx_wr & rx_wr;
-   assign txrd_wait = tx_rd & (tx_wr | rx_wr);
+   always @ (posedge clk)
+     if(access_forward)
+       begin
+	  write_out         <= mi_rx_en & write;
+	  datamode_out[1:0] <= datamode[1:0];
+	  ctrlmode_out[3:0] <= ctrlmode[3:0];
+	  dstaddr_out[31:0] <= mi_rx_en ? dstaddr[31:0] : srcaddr[31:0];
+	  data_out[31:0]    <= mi_rx_en ? data[31:0]    : mi_dout_mux[31:0];
+	  srcaddr_out[31:0] <= mi_rx_en ? srcaddr[31:0] : mi_dout_mux[63:32];
+       end
 
-   /********************************/
-   /*BASIC Register Interface      */
-   /********************************/
-   assign mi_basecfg_en = mi_en & (mi_addr[19:15]=={`EGROUP_TX,1'b0}) & (mi_addr[7:6]==`EBLOCK1);   
-   assign mi_mailbox_en = mi_en & (mi_addr[19:15]=={`EGROUP_RX,1'b0}) & (mi_addr[7:6]==`EBLOCK1);
-
-   /********************************/
-   /*TX Register Interface         */
-   /********************************/
-   assign mi_tx_cfg_en    = mi_en & (mi_addr[19:15]=={`EGROUP_TX,1'b0}) & (mi_addr[7:6]==`EBLOCK0);
-   assign mi_tx_mmu_en    = mi_en & (mi_addr[19:15]=={`EGROUP_RX,1'b1});
-
-   assign mi_tx_wen        = mi_din[63:0];
-   assign mi_tx_din[63:0]  = mi_din[63:0];
-   assign mi_tx_addr[19:0] = mi_addr[19:0];
-
-   /********************************/
-   /*RX Register Interface         */
-   /********************************/
-   assign mi_rx_cfg_en   = mi_en & (mi_addr[19:15]=={`EGROUP_RX,1'b0}) & (mi_addr[7:6]==`EBLOCK0);   
-   assign mi_rx_dma_en   = mi_en & (mi_addr[19:15]=={`EGROUP_RX,1'b0}) & (mi_addr[7:6]==`EBLOCK2);
-   assign mi_rx_mmu_en   = mi_en & (mi_addr[19:15]=={`EGROUP_RX,1'b1});
-   assign mi_rx_din[63:0]  = mi_din[63:0];
-   assign mi_rx_addr[19:0] = mi_addr[19:0];
-
-   //TODO: Do readback later....   
-//   
+   //Create packet
+   emesh2packet e2p (.packet_out	(packet_out[PW-1:0]),
+		     .access_in		(access_out),
+		     .write_in		(write_out),
+		     .datamode_in	(datamode_out[1:0]),
+		     .ctrlmode_in	(ctrlmode_out[3:0]),
+		     .dstaddr_in	(dstaddr_out[AW-1:0]),
+		     .data_in		(data_out[DW-1:0]),
+		     .srcaddr_in	(srcaddr_out[AW-1:0])
+		     );
+   
+   
 endmodule // ecfg_if
-// Local Variables:
-// verilog-library-directories:("." "../../common/hdl" "../../memory/hdl")
-// End:
-
-
 /*
-  Copyright (C) 2013 Adapteva, Inc.
+  Copyright (C) 2015 Adapteva, Inc.
   Contributed by Andreas Olofsson <andreas@adapteva.com>
  
-   This program is free software: you can redistribute it and/or modify
+  This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.This program is distributed in the hope 
@@ -206,4 +176,4 @@ endmodule // ecfg_if
   GNU General Public License for more details. You should have received a copy 
   of the GNU General Public License along with this program (see the file 
   COPYING).  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
