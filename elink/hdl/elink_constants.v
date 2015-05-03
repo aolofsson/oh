ebd9a89afdfb90d8289bae8e9f79b6815bffc0e3
Andreas Olofsson <andreas@adapteva.com>
2015-05-02 21:30:26 -0400

    Adding constants to handle model

--- elink/hdl/elink_constants.v
+++ elink/hdl/elink_constants.v
@@ -1,6 +1,7 @@
 //These constants are mutually exclusive
 `define TARGET_CLEAN
-//`define TARGET_XILINX
-`define AW 32
-`define DW 32
-`define LW 8
+`define CFG_FAKECLK   1      /*verilator doesn't get clock gating*/
+`define CFG_AW 32
+`define CFG_DW 32
+`define CFG_LW 8
+`define CFG_NW        13     /*Number of bytes in the transmission*/
