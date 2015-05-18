559ffcc6e01375af89b3382997e81ad7015c321d
Andreas Olofsson <andreas@adapteva.com>
2015-05-17 22:34:42 -0400

    File name changes and additions

--- elink/syn/xilinx/read_verilog.tcl
+++ elink/syn/xilinx/read_verilog.tcl
@@ -11,15 +11,19 @@ read_verilog $SRC/elink/hdl/elink_regmap.v
 read_verilog $SRC/elink/hdl/elink.v
 read_verilog $SRC/elink/hdl/eclocks.v
 read_verilog $SRC/elink/hdl/ereset.v
-read_verilog $SRC/elink/hdl/ecfg_clocks.v
+read_verilog $SRC/elink/hdl/ecfg_elink.v
 read_verilog $SRC/elink/hdl/ecfg_if.v
 read_verilog $SRC/elink/hdl/erx.v
+read_verilog $SRC/elink/hdl/erx_core.v
+read_verilog $SRC/elink/hdl/erx_fifo.v
 read_verilog $SRC/elink/hdl/erx_cfg.v
-read_verilog $SRC/elink/hdl/erx_disty.v
+read_verilog $SRC/elink/hdl/erx_arbiter.v
 read_verilog $SRC/elink/hdl/erx_protocol.v
 read_verilog $SRC/elink/hdl/erx_remap.v
 read_verilog $SRC/elink/hdl/erx_io.v
 read_verilog $SRC/elink/hdl/etx.v
+read_verilog $SRC/elink/hdl/etx_core.v
+read_verilog $SRC/elink/hdl/etx_fifo.v
 read_verilog $SRC/elink/hdl/etx_cfg.v
 read_verilog $SRC/elink/hdl/etx_arbiter.v
 read_verilog $SRC/elink/hdl/etx_protocol.v
@@ -29,6 +33,7 @@ read_verilog $SRC/elink/hdl/etx_io.v
 #COMMON
 read_verilog $SRC/common/hdl/toggle2pulse.v
 read_verilog $SRC/common/hdl/synchronizer.v
+read_verilog $SRC/common/hdl/pulse_stretcher.v
 read_verilog $SRC/common/hdl/clock_divider.v
 read_verilog $SRC/common/hdl/arbiter_priority.v
 
