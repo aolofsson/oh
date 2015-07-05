4df38ca35e531d8088e42b25392ec9d379f005fb
Ola Jeppsson <ola@adapteva.com>
2015-07-05 23:51:19 +0200

    elink: Update scripts
    Use paths relative top top_srcdir (so scripts can be run from any
    directory).
    Add missing files
    elink_example was renamed to axi_elink?
    
    Fails at placement.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- elink/scripts/xilinx/read_verilog.tcl
+++ elink/scripts/xilinx/read_verilog.tcl
@@ -1,54 +1,58 @@
-set SRC /home/aolofsson/Work_all/oh
+set pwd [file dirname [info script]]
+source $pwd/../../../include/oh.tcl
 
 #ONLY FOR REFERENCE EXAMPLE
-read_verilog $SRC/elink/hdl/axi_elink.v
+read_verilog $top_srcdir/elink/hdl/axi_elink.v
 
 #ELINK
-read_verilog $SRC/elink/hdl/elink_constants.v
-read_verilog $SRC/elink/hdl/elink_regmap.v
-read_verilog $SRC/elink/hdl/elink.v
-read_verilog $SRC/elink/hdl/eclocks.v
-read_verilog $SRC/elink/hdl/ereset.v
-read_verilog $SRC/elink/hdl/ecfg_elink.v
-read_verilog $SRC/elink/hdl/ecfg_if.v
-read_verilog $SRC/elink/hdl/erx.v
-read_verilog $SRC/elink/hdl/erx_core.v
-read_verilog $SRC/elink/hdl/erx_fifo.v
-read_verilog $SRC/elink/hdl/erx_cfg.v
-read_verilog $SRC/elink/hdl/erx_arbiter.v
-read_verilog $SRC/elink/hdl/erx_protocol.v
-read_verilog $SRC/elink/hdl/erx_remap.v
-read_verilog $SRC/elink/hdl/erx_io.v
-read_verilog $SRC/elink/hdl/etx.v
-read_verilog $SRC/elink/hdl/etx_core.v
-read_verilog $SRC/elink/hdl/etx_fifo.v
-read_verilog $SRC/elink/hdl/etx_cfg.v
-read_verilog $SRC/elink/hdl/etx_arbiter.v
-read_verilog $SRC/elink/hdl/etx_protocol.v
-read_verilog $SRC/elink/hdl/etx_remap.v
-read_verilog $SRC/elink/hdl/etx_io.v 
+read_verilog $top_srcdir/elink/hdl/elink_constants.v
+read_verilog $top_srcdir/elink/hdl/elink_regmap.v
+read_verilog $top_srcdir/elink/hdl/elink.v
+read_verilog $top_srcdir/elink/hdl/eclocks.v
+read_verilog $top_srcdir/elink/hdl/ereset.v
+read_verilog $top_srcdir/elink/hdl/ecfg_elink.v
+read_verilog $top_srcdir/elink/hdl/ecfg_if.v
+read_verilog $top_srcdir/elink/hdl/erx.v
+read_verilog $top_srcdir/elink/hdl/erx_core.v
+read_verilog $top_srcdir/elink/hdl/erx_fifo.v
+read_verilog $top_srcdir/elink/hdl/erx_cfg.v
+read_verilog $top_srcdir/elink/hdl/erx_arbiter.v
+read_verilog $top_srcdir/elink/hdl/erx_protocol.v
+read_verilog $top_srcdir/elink/hdl/erx_remap.v
+read_verilog $top_srcdir/elink/hdl/erx_io.v
+read_verilog $top_srcdir/elink/hdl/etx.v
+read_verilog $top_srcdir/elink/hdl/etx_core.v
+read_verilog $top_srcdir/elink/hdl/etx_fifo.v
+read_verilog $top_srcdir/elink/hdl/etx_cfg.v
+read_verilog $top_srcdir/elink/hdl/etx_arbiter.v
+read_verilog $top_srcdir/elink/hdl/etx_protocol.v
+read_verilog $top_srcdir/elink/hdl/etx_remap.v
+read_verilog $top_srcdir/elink/hdl/etx_io.v
+read_verilog $top_srcdir/elink/hdl/esaxi.v
+read_verilog $top_srcdir/elink/hdl/emaxi.v
 
 #COMMON
-read_verilog $SRC/common/hdl/toggle2pulse.v
-read_verilog $SRC/common/hdl/synchronizer.v
-read_verilog $SRC/common/hdl/pulse_stretcher.v
-read_verilog $SRC/common/hdl/clock_divider.v
-read_verilog $SRC/common/hdl/arbiter_priority.v
+read_verilog $top_srcdir/common/hdl/toggle2pulse.v
+read_verilog $top_srcdir/common/hdl/synchronizer.v
+read_verilog $top_srcdir/common/hdl/pulse_stretcher.v
+read_verilog $top_srcdir/common/hdl/clock_divider.v
+read_verilog $top_srcdir/common/hdl/arbiter_priority.v
 
 #EMESH
-read_verilog $SRC/emesh/hdl/emesh2packet.v
-read_verilog $SRC/emesh/hdl/packet2emesh.v
+read_verilog $top_srcdir/emesh/hdl/emesh2packet.v
+read_verilog $top_srcdir/emesh/hdl/packet2emesh.v
 
 #MEMORY/FIFO
-read_verilog $SRC/memory/hdl/fifo_async.v
-read_verilog $SRC/memory/hdl/fifo_cdc.v
-read_verilog $SRC/memory/hdl/memory_dp.v
-read_verilog $SRC/memory/hdl/memory_sp.v
-read_verilog $SRC/memory/hdl/fifo_full_block.v
-read_verilog $SRC/memory/hdl/fifo_empty_block.v
+read_verilog $top_srcdir/memory/hdl/fifo_async.v
+read_verilog $top_srcdir/memory/hdl/fifo_sync.v
+read_verilog $top_srcdir/memory/hdl/fifo_cdc.v
+read_verilog $top_srcdir/memory/hdl/memory_dp.v
+read_verilog $top_srcdir/memory/hdl/memory_sp.v
+read_verilog $top_srcdir/memory/hdl/fifo_full_block.v
+read_verilog $top_srcdir/memory/hdl/fifo_empty_block.v
 
 #MMU
-read_verilog $SRC/emmu/hdl/emmu.v
-read_verilog $SRC/emailbox/hdl/emailbox.v
-read_verilog $SRC/edma/hdl/edma.v
+read_verilog $top_srcdir/emmu/hdl/emmu.v
+read_verilog $top_srcdir/emailbox/hdl/emailbox.v
+read_verilog $top_srcdir/edma/hdl/edma.v
 
