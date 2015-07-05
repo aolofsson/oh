3db4e496755da82ea621e867f4adadd06864a7a5
Ola Jeppsson <ola@adapteva.com>
2015-07-05 19:43:32 +0200

    elink: Convert package_axi_elink to use helper script
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- elink/scripts/xilinx/package_axi_elink.tcl
+++ elink/scripts/xilinx/package_axi_elink.tcl
@@ -1,46 +1,29 @@
-########################################################
-set oh_path "../.."
+set pwd [file dirname [info script]]
+source $pwd/../../../include/oh.tcl
 
+# ???
 set_msg_config -id {ip_flow 19-459} -suppress
 
-########################################################
-#create a project
-set block axi_elink
-create_project $block . -force
+oh::ip::create axi_elink $top_builddir/axi_elink
 
-#######################################################
-#add files
-add_files -norecurse $oh_path/common/hdl
-add_files -norecurse $oh_path/emesh/hdl
-add_files -norecurse $oh_path/emmu/hdl/emmu.v
-add_files -norecurse $oh_path/edma/hdl/edma.v
-add_files -norecurse $oh_path/emailbox/hdl/emailbox.v
-add_files -norecurse $oh_path/elink/hdl
+set elink_src_files [list \
+    "$top_srcdir/common/hdl" \
+    "$top_srcdir/emesh/hdl" \
+    "$top_srcdir/emmu/hdl/emmu.v" \
+    "$top_srcdir/edma/hdl/edma.v" \
+    "$top_srcdir/emailbox/hdl/emailbox.v" \
+    "$top_srcdir/elink/hdl" ]
+set elink_constr_files [list \
+    "$top_srcdir/elink/scripts/xilinx/elink_clocks.xdc" \
+    "$top_srcdir/elink/scripts/xilinx/elink_pins.xdc" \
+    "$top_srcdir/elink/scripts/xilinx/elink_timing.xdc" ]
+set elink_ip_files [concat $elink_src_files $elink_constr_files]
 
-#######################################################
-#Package IP
-ipx::package_project -root_dir .
+oh::ip::add_files axi_elink $elink_ip_files
+# Does not work / is it needed ?
+#oh::ip::add_constraints $elink_constr_files
 
-#######################################################
-#Vendor settings
-set_property vendor {www.parallella.org} [ipx::current_core]
-set_property library {user} [ipx::current_core]
-set_property taxonomy {{/AXI_Infrastructure}} [ipx::current_core]
-set_property vendor_display_name {OH!} [ipx::current_core]
-set_property company_url {www.parallella.org} [ipx::current_core]
+oh::ip::set_properties $top_builddir/axi_elink
 
-#######################################################
-#Device Families Supported
-set_property supported_families \
-    {
-     {virtex7}    {Production} \
-     {kintex7}    {Production} \
-     {artix7}     {Production} \
-     {zynq}       {Production}} \
-  [ipx::current_core]
-
-#######################################################
-#Save files
 ipx::save_core [ipx::current_core]
-
 
