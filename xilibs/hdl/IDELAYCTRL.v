c60f9236da574693c10447cc39c6acf5eb20501b
Andreas Olofsson <andreas@adapteva.com>
2015-10-07 19:18:54 -0400

    Adding hack model for RDY signal -should probably last for more cycles thatn this?

--- xilibs/hdl/IDELAYCTRL.v
+++ xilibs/hdl/IDELAYCTRL.v
@@ -6,10 +6,18 @@ module IDELAYCTRL (/*AUTOARG*/
    REFCLK, RST
    );
 
-    output  RDY;    //goes high when delay has been calibrated
-    input   REFCLK; //reference clock for setting tap delay
-    input   RST;    //reset pulse for setting 
-
+   output  RDY;    //goes high when delay has been calibrated
+   input   REFCLK; //reference clock for setting tap delay
+   input   RST;    //reset pulse for setting 
+   
+   reg 	    RDY;
+   
+   always @ (posedge REFCLK or posedge RST)
+     if(RST)
+       RDY <= 1'b0;
+     else
+       RDY <= 1'b1; //one clock cycle on REFCLK
+   
 endmodule // IDELAYCTRL
 
 
