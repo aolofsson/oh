92272e211d6205ea6894c55f7fd633d0af7868f5
Andreas Olofsson <andreas@adapteva.com>
2015-11-04 20:04:44 -0500

    Adding missind dirs in comamnd file

--- elink/dv/build.sh
+++ elink/dv/build.sh
@@ -8,7 +8,9 @@
 #
 dut="elink"
 top="../../common/dv/dv_top.v"
-iverilog -g2005 -DTARGET_SIMPLE=1 -DTARGET_XILINX=1 $top dut_${dut}.v -f ../../common/dv/libs.cmd -o ${dut}.vvp -pfileline=1
+iverilog -g2005 -DTARGET_SIMPLE=1 -DTARGET_XILINX=1 $top dut_${dut}.v -f ../../common/dv/libs.cmd -o ${dut}.vvp 
 
+
+#-pfileline=1
 #-Wall
 
