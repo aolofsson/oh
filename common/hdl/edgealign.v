4b384be602c35aef00bcda3601df7e47d2f72e59
Andreas Olofsson <andreas@adapteva.com>
2015-11-14 23:33:48 -0500

    Fixing edge align circuit - duh error - making output positive edge aligned, the negedge nastyness should be maintained within module...

--- common/hdl/edgealign.v
+++ common/hdl/edgealign.v
@@ -1,19 +1,22 @@
 /* Detects the common aligned positive edge for a 
- * slow/fast clocks
+ * slow/fast clocks. The circuit uses the negedge of the fast clock
+ * to sample the slow clock. Output is positive edge sampled.
  * 
  * NOTE: Assumes clocks are aligned and synchronous!
  *
  *    ___________             ___________
- * __/           \___________/           \  SLOWCLK
+ * __/           \___________/           \   SLOWCLK
  *    __    __    __    __    __    __ 
- *  _/  \__/  \__/  \__/  \__/  \__/  \__/  FASTCLK
- *       ___________             _________   
- *    __/           \___________/           CLK45
- *             ___________             ___   
- *    ________/           \___________/     CLK135 
+ *  _/  \__/  \__/  \__/  \__/  \__/  \__/   FASTCLK
+ *        ___________              _________   
+ *    ___/ 1     1   \_0_____0____/          CLK45
+ *           ____________              ___   
+ *    ______/    1     1 \___0____0___/      CLK90 
  * 
- * ____                    ______               
- *     \__________________/      \________  FIRSTEDGE 
+ * ____                  ______               
+ *     \________________/      \________     FIRSTEDGE 
+ * 
+ *                      
  * 
  */
 
@@ -29,16 +32,18 @@ module edgealign (/*AUTOARG*/
    output firstedge;
 
    reg 	  clk45;
-   reg 	  clk135;
+   reg 	  clk90;
    reg 	  firstedge;
 
    always @ (negedge fastclk) 
-     begin
-	clk45     <= slowclk;
-	clk135    <= clk45;
-	firstedge <= ~clk45 & ~clk135;
-     end
+     clk45     <= slowclk;
 
+   always @ (posedge fastclk) 
+     begin
+	clk90     <= clk45;
+	firstedge <= ~clk45 & clk90;
+     end
+   
   //TODO: parametrized based on 1/N ratios?
    
 endmodule // edgealign
