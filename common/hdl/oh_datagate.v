eb8f6c1f51f3dd6d3c61cfa905a2e79b385a8033
Andreas Olofsson <andreas@adapteva.com>
2015-12-03 18:05:08 -0500

    Adding datagate power saving module

---
+++ common/hdl/oh_datagate.v
@@ -0,0 +1,26 @@
+module oh_datagate (/*AUTOARG*/
+   // Outputs
+   dout,
+   // Inputs
+   clk, en, din
+   );
+
+   parameter DW = 32;
+   parameter PS = 3;
+   
+   input           clk;
+   input           en;
+   input [DW-1:0]  din;   
+   output [DW-1:0] dout;	  
+	 
+   reg [PS-1:0]    enable_pipe;   
+
+   always @ (posedge clk)
+     enable_pipe[PS-1:0] <= {enable_pipe[PS-2:0],en};
+   
+   assign enable = {enable_pipe[PS-1:0],en};
+
+   assign dout[DW-1:0] =  {(DW){enable}} & din[DW-1:0];
+   
+   
+endmodule // oh_datagate
