2a70178f47c69f3621fe194904c6d903e27fd7fb
Andreas Olofsson <andreas@adapteva.com>
2015-12-17 12:33:19 -0500

    Changing core parameter of packets -Address width is a more natural primary component

--- emesh/hdl/emesh2packet.v
+++ emesh/hdl/emesh2packet.v
@@ -1,32 +1,54 @@
-/*Converts an emesh bundle into a 104 bit packet*/
+/*
+
+ * ---- E4 PACKET FORMAT (104 bits) ----
+ * [1]       write bit
+ * [2:1]     datamode
+ * [6:3]     ctrlmode
+ * [7]       reserved
+ * [39:8]    f0 = dstaddr(lo)
+ * [71:40]   f1 = data (lo)
+ * [103:72]  f2 = srcaddr(lo) /  data (hi)
+ * 
+ */
 module emesh2packet(/*AUTOARG*/
    // Outputs
    packet_out,
    // Inputs
-   write_in, datamode_in, ctrlmode_in, dstaddr_in, data_in,
-   srcaddr_in
+   write_out, datamode_out, ctrlmode_out, dstaddr_out, data_out,
+   srcaddr_out
    );
+   parameter AW      = 32;   
+   localparam PW     = (2*AW+40); 
 
-   parameter AW=32;
-   parameter DW=32;
-   parameter PW=104;
    
    //Emesh signal bundle
-   input 	    write_in;   
-   input [1:0] 	    datamode_in;
-   input [3:0] 	    ctrlmode_in;
-   input [AW-1:0]   dstaddr_in;
-   input [DW-1:0]   data_in;   
-   input [AW-1:0]   srcaddr_in;   
+   input 	    write_out;   
+   input [1:0] 	    datamode_out;
+   input [4:0] 	    ctrlmode_out;
+   input [AW-1:0]   dstaddr_out;
+   input [AW-1:0]   data_out;   
+   input [AW-1:0]   srcaddr_out;   
    
    //Output packet
    output [PW-1:0]  packet_out;
 
-   assign packet_out[0]       = write_in;   
-   assign packet_out[2:1]     = datamode_in[1:0];
-   assign packet_out[7:3]     = {1'b0,ctrlmode_in[3:0]};
-   assign packet_out[39:8]    = dstaddr_in[AW-1:0];
-   assign packet_out[71:40]   = data_in[AW-1:0];
-   assign packet_out[103:72]  = srcaddr_in[AW-1:0];
-     
+   
+   assign packet_out[0]       = write_out;   
+   assign packet_out[2:1]     = datamode_out[1:0];
+   assign packet_out[7:3]     = ctrlmode_out[4:0];
+   
+   generate   
+      if(AW==32 & PW==104)
+	begin	  
+	   assign packet_out[39:8]    = dstaddr_out[AW-1:0];
+	   assign packet_out[71:40]   = data_out[AW-1:0];
+	   assign packet_out[103:72]  = srcaddr_out[AW-1:0];
+	end 
+      else
+	begin
+	   initial
+	     $display ("Only AW=32 and PW=104 is supported");
+	end
+   endgenerate
 endmodule // emesh2packet
+
--- emesh/hdl/packet2emesh.v
+++ emesh/hdl/packet2emesh.v
@@ -1,32 +1,56 @@
+/*
+ * ---- E4 PACKET FORMAT (104 bits) ----
+ * [0]       write bit
+ * [2:1]     datamode
+ * [6:3]     ctrlmode
+ * [7]       reserved
+ * [39:8]    f0 = dstaddr(lo)
+ * [71:40]   f1 = data (lo)
+ * [103:72]  f2 = srcaddr(lo) / data (hi)
+ * 
+ */
+
 module packet2emesh(/*AUTOARG*/
    // Outputs
-   write_out, datamode_out, ctrlmode_out, data_out, dstaddr_out,
-   srcaddr_out,
+   write_in, datamode_in, ctrlmode_in, dstaddr_in, srcaddr_in,
+   data_in,
    // Inputs
    packet_in
    );
 
-   parameter PW      = 104;   //packet width
-   parameter DW      = 32;    //data width
-   parameter AW      = 32;    //addess width
+   parameter AW     = 32;   
+   parameter PW     = (2*AW+40); 
 
    //Input packet
    input [PW-1:0]   packet_in;
 
    //Emesh signal bundle 
-   output 	        write_out;
-   output [1:0] 	datamode_out;
-   output [3:0] 	ctrlmode_out;
-   output [DW-1:0] 	data_out; //TODO: fix to make relative to PW
-   output [AW-1:0]      dstaddr_out;
-   output [AW-1:0]      srcaddr_out;
-      
-   assign write_out             = packet_in[0];   
-   assign datamode_out[1:0]     = packet_in[2:1];   
-   assign ctrlmode_out[3:0]     = packet_in[6:3];   
-   assign dstaddr_out[31:0]     = packet_in[39:8]; 	 
-   assign srcaddr_out[31:0]     = packet_in[103:72];  
-   assign data_out[31:0]        = packet_in[71:40];  
-      
+   output 	     write_in;
+   output [1:0]      datamode_in;
+   output [4:0]      ctrlmode_in;
+   output [AW-1:0]   dstaddr_in;
+   output [AW-1:0]   srcaddr_in;
+   output [AW-1:0]   data_in;
+
+   generate
+      if(AW==32 & PW==104)
+	begin
+	   assign write_in           = packet_in[0];   
+	   assign datamode_in[1:0]   = packet_in[2:1];   
+	   assign ctrlmode_in[4:0]   = {1'b0,packet_in[6:3]};   
+	   assign dstaddr_in[31:0]   = packet_in[39:8]; 	 
+	   assign srcaddr_in[31:0]   = packet_in[103:72];  
+	   assign data_in[63:0]      = packet_in[103:40];  
+	end
+      else
+	begin
+	   initial
+	     $display ("Only AW=32 and PW=104 is supported");
+	end
+   endgenerate
+   
 endmodule // packet2emesh
+
+
+
 
