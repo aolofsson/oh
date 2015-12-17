167744d7a9eea6241c5e187519f8b462619b53e9
Andreas Olofsson <andreas@adapteva.com>
2015-12-17 12:52:49 -0500

    Fixed data width

--- emesh/hdl/packet2emesh.v
+++ emesh/hdl/packet2emesh.v
@@ -40,7 +40,7 @@ module packet2emesh(/*AUTOARG*/
 	   assign ctrlmode_in[4:0]   = {1'b0,packet_in[6:3]};   
 	   assign dstaddr_in[31:0]   = packet_in[39:8]; 	 
 	   assign srcaddr_in[31:0]   = packet_in[103:72];  
-	   assign data_in[63:0]      = packet_in[103:40];  
+	   assign data_in[31:0]      = packet_in[71:40];  
 	end
       else
 	begin
