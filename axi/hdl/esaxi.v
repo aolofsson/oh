8852b8db99715abc22bc71e78e99a3606ba028d4
Andreas Olofsson <andreas@adapteva.com>
2016-01-19 13:26:42 -0500

    Changing timeout value -timeout was hit in sim, 8bits is too short -refactoring to simplify names, rx/tx was confusing

--- axi/hdl/esaxi.v
+++ axi/hdl/esaxi.v
@@ -1,53 +1,51 @@
 module esaxi (/*autoarg*/
    // Outputs
-   txwr_access, txwr_packet, txrd_access, txrd_packet, rxrr_wait,
-   s_axi_arready, s_axi_awready, s_axi_bid, s_axi_bresp, s_axi_bvalid,
-   s_axi_rid, s_axi_rdata, s_axi_rlast, s_axi_rresp, s_axi_rvalid,
-   s_axi_wready,
+   wr_access, wr_packet, rd_access, rd_packet, rr_wait, s_axi_arready,
+   s_axi_awready, s_axi_bid, s_axi_bresp, s_axi_bvalid, s_axi_rid,
+   s_axi_rdata, s_axi_rlast, s_axi_rresp, s_axi_rvalid, s_axi_wready,
    // Inputs
-   txwr_wait, txrd_wait, rxrr_access, rxrr_packet, s_axi_aclk,
-   s_axi_aresetn, s_axi_arid, s_axi_araddr, s_axi_arburst,
-   s_axi_arcache, s_axi_arlock, s_axi_arlen, s_axi_arprot,
-   s_axi_arqos, s_axi_arsize, s_axi_arvalid, s_axi_awid, s_axi_awaddr,
-   s_axi_awburst, s_axi_awcache, s_axi_awlock, s_axi_awlen,
-   s_axi_awprot, s_axi_awqos, s_axi_awsize, s_axi_awvalid,
-   s_axi_bready, s_axi_rready, s_axi_wid, s_axi_wdata, s_axi_wlast,
-   s_axi_wstrb, s_axi_wvalid
+   wr_wait, rd_wait, rr_access, rr_packet, s_axi_aclk, s_axi_aresetn,
+   s_axi_arid, s_axi_araddr, s_axi_arburst, s_axi_arcache,
+   s_axi_arlock, s_axi_arlen, s_axi_arprot, s_axi_arqos, s_axi_arsize,
+   s_axi_arvalid, s_axi_awid, s_axi_awaddr, s_axi_awburst,
+   s_axi_awcache, s_axi_awlock, s_axi_awlen, s_axi_awprot,
+   s_axi_awqos, s_axi_awsize, s_axi_awvalid, s_axi_bready,
+   s_axi_rready, s_axi_wid, s_axi_wdata, s_axi_wlast, s_axi_wstrb,
+   s_axi_wvalid
    );
    
-   parameter          ID                  = 12'h999;   
    parameter          S_IDW               = 12;
    parameter          PW                  = 104;
-   parameter [AW-1:0] RETURN_ADDR         = {ID,20'h0};
+   parameter [AW-1:0] RETURN_ADDR         = 0;
    parameter          AW                  = 32;
    parameter          DW                  = 32;
 
 `ifdef TARGET_SIM
-   parameter         TW                  = 8;   //timeout counter width
+   parameter         TW                  = 16;   //timeout counter width
 `else
    parameter         TW                  = 16;  //timeout counter width
 `endif
  
    //#############################
-   //# Write request (outbound)
+   //# Write request
    //#############################
-   output 	   txwr_access;   
-   output [PW-1:0] txwr_packet;
-   input 	   txwr_wait;
+   output 	   wr_access;   
+   output [PW-1:0] wr_packet;
+   input 	   wr_wait;
    
    //#############################
-   //# Read request (outbound)
+   //# Read request
    //#############################
-   output 	   txrd_access;   
-   output [PW-1:0] txrd_packet;
-   input 	   txrd_wait;
+   output 	   rd_access;   
+   output [PW-1:0] rd_packet;
+   input 	   rd_wait;
    
    //#############################
-   //# Read response (inbound)
+   //# Read response
    //#############################
-   input 	   rxrr_access;         
-   input [PW-1:0]  rxrr_packet;
-   output 	   rxrr_wait;
+   input 	   rr_access;         
+   input [PW-1:0]  rr_packet;
+   output 	   rr_wait;
 
    //#############################
    //# AXI Slave Interface
@@ -136,19 +134,19 @@ module esaxi (/*autoarg*/
    reg 		      write_active;
    reg 		      b_wait;      // waiting to issue write response (unlikely?)
    
-   reg 		      txwr_access;
-   reg [1:0] 	      txwr_datamode;
-   reg [31:0] 	      txwr_dstaddr;
-   reg [31:0] 	      txwr_data;
+   reg 		      wr_access;
+   reg [1:0] 	      wr_datamode;
+   reg [31:0] 	      wr_dstaddr;
+   reg [31:0] 	      wr_data;
 
-   reg [31:0] 	      txwr_data_reg;
-   reg [31:0] 	      txwr_dstaddr_reg;
-   reg [1:0] 	      txwr_datamode_reg;
+   reg [31:0] 	      wr_data_reg;
+   reg [31:0] 	      wr_dstaddr_reg;
+   reg [1:0] 	      wr_datamode_reg;
    
-   reg 		      txrd_access;
-   reg [1:0] 	      txrd_datamode;
-   reg [31:0] 	      txrd_dstaddr;
-   reg [31:0] 	      txrd_srcaddr;  //read reaspne address
+   reg 		      rd_access;
+   reg [1:0] 	      rd_datamode;
+   reg [31:0] 	      rd_dstaddr;
+   reg [31:0] 	      rd_srcaddr;  //read reaspne address
    
    reg 		      pre_wr_en;    // delay for data alignment
    
@@ -158,51 +156,51 @@ module esaxi (/*autoarg*/
    wire 	      last_wr_beat;
    wire 	      last_rd_beat;
   
-   wire [31:0] 	      rxrr_mux_data;
-   wire [DW-1:0]      rxrr_data;
-   wire [31:0] 	      rxrr_return_data;
+   wire [31:0] 	      rr_mux_data;
+   wire [DW-1:0]      rr_data;
+   wire [31:0] 	      rr_return_data;
    reg [TW-1:0]       timeout_counter;
    
    //###################################################
    //#PACKET TO MESH
    //###################################################
 
-   //TXWR
-   emesh2packet e2p_txwr (
+   //WR
+   emesh2packet e2p_wr (
 		     // Outputs
-		     .packet_out	(txwr_packet[PW-1:0]),
+		     .packet_out	(wr_packet[PW-1:0]),
 		     // Inputs
 		     .write_out		(1'b1),
-		     .datamode_out	(txwr_datamode[1:0]),
+		     .datamode_out	(wr_datamode[1:0]),
 		     .ctrlmode_out	(5'b0),
-		     .dstaddr_out	(txwr_dstaddr[AW-1:0]),
-		     .data_out		(txwr_data[DW-1:0]),
+		     .dstaddr_out	(wr_dstaddr[AW-1:0]),
+		     .data_out		(wr_data[DW-1:0]),
 		     .srcaddr_out	(32'b0)//only 32b slave write supported
 		     );
 
-   //TXRD
-   emesh2packet e2p_txrd (
+   //RD
+   emesh2packet e2p_rd (
 		     // Outputs
-		     .packet_out	(txrd_packet[PW-1:0]),
+		     .packet_out	(rd_packet[PW-1:0]),
 		     // Inputs
 		     .write_out		(1'b0),
-		     .datamode_out	(txrd_datamode[1:0]),
+		     .datamode_out	(rd_datamode[1:0]),
 		     .ctrlmode_out	(5'b0),
-		     .dstaddr_out	(txrd_dstaddr[AW-1:0]),
+		     .dstaddr_out	(rd_dstaddr[AW-1:0]),
 		     .data_out		(32'b0),
-		     .srcaddr_out	(txrd_srcaddr[AW-1:0])
+		     .srcaddr_out	(rd_srcaddr[AW-1:0])
 		     );   
-   //RXRR
-   packet2emesh p2e_rxrr (
+   //RR
+   packet2emesh p2e_rr (
 			  // Outputs
 			  .write_in		(),
 			  .datamode_in		(),
 			  .ctrlmode_in		(),
 			  .dstaddr_in		(),
-			  .data_in		(rxrr_data[DW-1:0]),
+			  .data_in		(rr_data[DW-1:0]),
 			  .srcaddr_in		(),
 			  // Inputs
-			  .packet_in		(rxrr_packet[PW-1:0])
+			  .packet_in		(rr_packet[PW-1:0])
 			  );
 
    //###################################################
@@ -278,7 +276,7 @@ module esaxi (/*autoarg*/
 	  if( last_wr_beat )
 	    s_axi_wready <= 1'b0;
 	  else if( write_active )
-	    s_axi_wready <= ~txwr_wait;
+	    s_axi_wready <= ~wr_wait;
        end                             
    
    always @( posedge s_axi_aclk )
@@ -374,47 +372,47 @@ module esaxi (/*autoarg*/
    always @( posedge s_axi_aclk ) 
      if (~s_axi_aresetn) 
        begin
-          txwr_data_reg[31:0]     <= 32'd0;	  
-          txwr_dstaddr_reg[31:0]  <= 32'd0;	 
-          txwr_datamode_reg[1:0]  <= 2'd0;
-          txwr_access             <= 1'b0;
+          wr_data_reg[31:0]     <= 32'd0;	  
+          wr_dstaddr_reg[31:0]  <= 32'd0;	 
+          wr_datamode_reg[1:0]  <= 2'd0;
+          wr_access             <= 1'b0;
           pre_wr_en               <= 1'b0;
        end 
      else 
        begin
 	  pre_wr_en                 <= s_axi_wready & s_axi_wvalid;
-          txwr_access               <= pre_wr_en;
-	  txwr_datamode_reg[1:0]    <= axi_awsize[1:0];	
-          txwr_dstaddr_reg[31:2]    <= axi_awaddr[31:2]; //set lsbs of address based on write strobes	 
+          wr_access               <= pre_wr_en;
+	  wr_datamode_reg[1:0]    <= axi_awsize[1:0];	
+          wr_dstaddr_reg[31:2]    <= axi_awaddr[31:2]; //set lsbs of address based on write strobes	 
 	  //What is up with this logic??
 	  if(s_axi_wstrb[0])//| (axi_awsize[1:0]==2'b10)32-bits
 	    begin
-	       txwr_data_reg[31:0]   <= s_axi_wdata[31:0];
-	       txwr_dstaddr_reg[1:0] <= 2'd0;
+	       wr_data_reg[31:0]   <= s_axi_wdata[31:0];
+	       wr_dstaddr_reg[1:0] <= 2'd0;
 	    end
 	  else if(s_axi_wstrb[1])
 	    begin
-	       txwr_data_reg[31:0]   <= {8'd0, s_axi_wdata[31:8]};
-	       txwr_dstaddr_reg[1:0] <= 2'd1;
+	       wr_data_reg[31:0]   <= {8'd0, s_axi_wdata[31:8]};
+	       wr_dstaddr_reg[1:0] <= 2'd1;
 	    end
 	  else if(s_axi_wstrb[2])
 	    begin
-	       txwr_data_reg[31:0]   <= {16'd0, s_axi_wdata[31:16]};
-	       txwr_dstaddr_reg[1:0] <= 2'd2;
+	       wr_data_reg[31:0]   <= {16'd0, s_axi_wdata[31:16]};
+	       wr_dstaddr_reg[1:0] <= 2'd2;
 	    end
 	  else
 	    begin
-	       txwr_data_reg[31:0]   <= {24'd0, s_axi_wdata[31:24]};
-	       txwr_dstaddr_reg[1:0] <= 2'd3;
+	       wr_data_reg[31:0]   <= {24'd0, s_axi_wdata[31:24]};
+	       wr_dstaddr_reg[1:0] <= 2'd3;
 	    end
        end // else: !if(~s_axi_aresetn)
 
    //Pipeline stage!
    always @( posedge s_axi_aclk )     
      begin
-        txwr_data[31:0]     <= txwr_data_reg[31:0];	  
-        txwr_dstaddr[31:0]  <= txwr_dstaddr_reg[31:0];	  
-        txwr_datamode[1:0]  <= txwr_datamode_reg[1:0];	  
+        wr_data[31:0]     <= wr_data_reg[31:0];	  
+        wr_dstaddr[31:0]  <= wr_dstaddr_reg[31:0];	  
+        wr_datamode[1:0]  <= wr_datamode_reg[1:0];	  
      end
    
    //###################################################
@@ -427,26 +425,26 @@ module esaxi (/*autoarg*/
    // -- because elink reads are not generally 
    // -- returned in order, we will only allow
    // -- one at a time.
-   //Need to look at txrd_wait signal
+   //Need to look at rd_wait signal
    
    always @( posedge s_axi_aclk )
      if (~s_axi_aresetn) 
        begin
-	  txrd_access         <= 1'b0;      
-	  txrd_datamode[1:0]  <= 2'd0;
-	  txrd_dstaddr[31:0]  <= 32'd0;
-	  txrd_srcaddr[31:0]  <= 32'd0;	 
-          ractive_reg         <= 1'b0;
-          rnext               <= 1'b0;          
+	  rd_access         <= 1'b0;      
+	  rd_datamode[1:0]  <= 2'd0;
+	  rd_dstaddr[31:0]  <= 32'd0;
+	  rd_srcaddr[31:0]  <= 32'd0;	 
+          ractive_reg       <= 1'b0;
+          rnext             <= 1'b0;          
       end 
      else
        begin
-          ractive_reg         <= read_active;
-          rnext               <= s_axi_rvalid & s_axi_rready & ~s_axi_rlast;    
-          txrd_access         <= ( ~ractive_reg & read_active ) | rnext;       
-	  txrd_datamode[1:0]  <= axi_arsize[1:0];
-	  txrd_dstaddr[31:0]  <= axi_araddr[31:0];
-	  txrd_srcaddr[31:0]  <= RETURN_ADDR;
+          ractive_reg       <= read_active;
+          rnext             <= s_axi_rvalid & s_axi_rready & ~s_axi_rlast;    
+          rd_access         <= ( ~ractive_reg & read_active ) | rnext;       
+	  rd_datamode[1:0]  <= axi_arsize[1:0];
+	  rd_dstaddr[31:0]  <= axi_araddr[31:0];
+	  rd_srcaddr[31:0]  <= RETURN_ADDR;
 	  //TODO: use arid+srcaddr for out of order ?
        end
 
@@ -456,12 +454,12 @@ module esaxi (/*autoarg*/
    //Read response AXI state machine
    //Only one outstanding read
 
-   assign rxrr_wait = 1'b0;
+   assign rr_wait = 1'b0;
 
 
-   assign rxrr_return_access     = rxrr_access | rxrr_timeout_access;
-   assign rxrr_return_data[31:0] = rxrr_timeout_access ? 32'hDEADBEEF :
-				                         rxrr_data[31:0];
+   assign rr_return_access     = rr_access | rr_timeout_access;
+   assign rr_return_data[31:0] = rr_timeout_access ? 32'hDEADBEEF :
+				                         rr_data[31:0];
    
    always @( posedge s_axi_aclk ) 
       if (!s_axi_aresetn) 
@@ -472,14 +470,14 @@ module esaxi (/*autoarg*/
 	end 
       else 
 	begin
-         if( rxrr_return_access ) 
+         if( rr_return_access ) 
 	   begin
               s_axi_rvalid <= 1'b1;
-              s_axi_rresp  <= rxrr_timeout_access ? 2'b10 : 2'b00;
+              s_axi_rresp  <= rr_timeout_access ? 2'b10 : 2'b00;
             case( axi_arsize[1:0] )
-              2'b00:   s_axi_rdata[31:0] <= {4{rxrr_return_data[7:0]}};  //8-bit
-              2'b01:   s_axi_rdata[31:0] <= {2{rxrr_return_data[15:0]}}; //16-bit
-              default: s_axi_rdata[31:0] <= rxrr_return_data[31:0];      //32-bit
+              2'b00:   s_axi_rdata[31:0] <= {4{rr_return_data[7:0]}};  //8-bit
+              2'b01:   s_axi_rdata[31:0] <= {2{rr_return_data[15:0]}}; //16-bit
+              default: s_axi_rdata[31:0] <= rr_return_data[31:0];      //32-bit
             endcase // case ( axi_arsize[1:0] )
            end 
 	 else if( s_axi_rready ) 
@@ -502,7 +500,7 @@ module esaxi (/*autoarg*/
        case(timeout_state[1:0])
 	 `TIMEOUT_IDLE    : timeout_state[1:0] <= (s_axi_arvalid & s_axi_arready ) ? `TIMEOUT_ARMED : 
 						                                     `TIMEOUT_IDLE;
-	 `TIMEOUT_ARMED   : timeout_state[1:0] <=  rxrr_access     ? `TIMEOUT_IDLE    :
+	 `TIMEOUT_ARMED   : timeout_state[1:0] <=  rr_access     ? `TIMEOUT_IDLE    :
 						   counter_expired ? `TIMEOUT_EXPIRED : 
 						                     `TIMEOUT_ARMED;
 	 `TIMEOUT_EXPIRED : timeout_state[1:0] <= `TIMEOUT_IDLE;	 
@@ -517,7 +515,7 @@ module esaxi (/*autoarg*/
        timeout_counter[TW-1:0] <= timeout_counter[TW-1:0] - 1'b1;
 
    assign counter_expired     = ~(|timeout_counter[TW-1:0]);
-   assign rxrr_timeout_access = (timeout_state[1:0]==`TIMEOUT_EXPIRED);
+   assign rr_timeout_access = (timeout_state[1:0]==`TIMEOUT_EXPIRED);
         
 endmodule // esaxi
 
