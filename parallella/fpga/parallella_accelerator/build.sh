5b15aa2b79b3b97876a5626d0940cd8a511386f9
Andreas Olofsson <andreas@adapteva.com>
2016-01-20 17:36:57 -0500

    Adding IP packaging files

---
+++ parallella/fpga/parallella_accelerator/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+vivado -mode batch -source run.tcl
+
---
+++ parallella/fpga/parallella_accelerator/system_params.tcl
@@ -0,0 +1,29 @@
+# NOTE: See UG1118 for more information
+
+#########################################
+# VARIABLES
+#########################################
+set design parallella_base
+set projdir ./
+set root "../../.."
+set partname "xc7z020clg400-1"
+
+set hdl_files [list \
+                   $root/parallella/hdl/parallella_accelerator.v \
+	           $root/parallella/hdl \
+	           $root/accelerator/hdl \
+		   $root/common/hdl/ \
+		   $root/emesh/hdl \
+		   $root/emmu/hdl \
+		   $root/axi/hdl \
+		   $root/emailbox/hdl \
+		   $root/edma/hdl \
+	           $root/elink/hdl \
+		  ]
+
+set ip_files   [list \
+		    $root/xilibs/ip/fifo_async_104x32.xci \
+		   ]
+
+set constraints_files []
+
