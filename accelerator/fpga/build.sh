d614b4f33b62059e370b7f8013d27bd756e09b65
Andreas Olofsson <andreas@adapteva.com>
2016-01-20 18:45:20 -0500

    Adding FPGA build files

--- accelerator/fpga/build.sh
+++ accelerator/fpga/build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-vivado -mode batch -source package_ip.tcl
+
+#clean up
+rm system_wrapper.bit.bin bit2bin.bin
+
+#package IP
+vivado -mode batch -source package.tcl
+
+#create bit stream
+vivado -mode batch -source run.tcl
+
+#xilinx stuff...
+bootgen -image bit2bin.bif -split bin 
+cp system_wrapper.bit.bin parallella.bit.bin
+
--- accelerator/fpga/package.tcl
+++ accelerator/fpga/package.tcl
@@ -1,3 +1,3 @@
 #STEP1: DEFINE KEY PARAMETERS
-source ./system_params.tcl
+source ./ip_params.tcl
 source ../../common/fpga/create_ip.tcl
