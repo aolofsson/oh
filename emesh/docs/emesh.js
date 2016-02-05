4512f04e22ca9ce4e726feb0e8c44a0c496f5547
Andreas Olofsson <andreas@adapteva.com>
2016-02-04 20:45:31 -0500

    Adding emesh timing diagram

---
+++ emesh/docs/emesh.js
@@ -0,0 +1,6 @@
+{ "signal" : [
+  { "name": "clk",    "wave": "P......" },
+  { "name": "access", "wave": "0.1...0" },
+  { "name": "packet", "wave": "xx===.x", "data": ["AA", "BB", "CC", "data"] },
+  { "name": "wait", "wave": "0...10" }
+]}
