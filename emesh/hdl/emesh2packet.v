b5fd4155af3c15f3dbff51329709761633114ebc
Andreas Olofsson <andreas@adapteva.com>
2016-02-18 12:38:53 -0500

    Adding 16bit support (dummy)

--- emesh/hdl/emesh2packet.v
+++ emesh/hdl/emesh2packet.v
@@ -53,14 +53,20 @@ module emesh2packet(/*AUTOARG*/
 	   assign packet_out[135:104] = srcaddr_out[63:32];
 	   assign packet_out[167:136] = dstaddr_out[63:32];
 	end
-      else
+      else if(AW==32)
 	begin : packet32
 	   assign packet_out[39:8]    = dstaddr_out[31:0];
 	   assign packet_out[71:40]   = data_out[31:0];
 	   assign packet_out[103:72]  = srcaddr_out[31:0];
-	end      
+	end
+      else if(AW==16)
+	begin : packet16
+	   assign packet_out[23:8]    = dstaddr_out[15:0];
+	   assign packet_out[39:24]   = data_out[15:0];
+	   assign packet_out[55:40]   = srcaddr_out[15:0];
+	end
+     
    endgenerate
-
-
+   
 endmodule // emesh2packet
 
