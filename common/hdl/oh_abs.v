1c0646c569af2ec80ef1b945bab0e455d8234516
Andreas Olofsson <andreas@adapteva.com>
2016-02-25 15:00:33 -0500

    Implemented absolute value function

--- common/hdl/oh_abs.v
+++ common/hdl/oh_abs.v
@@ -1,20 +1,24 @@
-module oh_abs (/*AUTOARG*/);
-
-   //###############################################################
-   //# Parameters
-   //###############################################################
-   parameter DW = 64;
-          
-   //###############################################################
-   //# Interface
-   //###############################################################
+module oh_abs (/*AUTOARG*/
+   // Outputs
+   out, overflow,
+   // Inputs
+   in
+   );
+ 
+   parameter DW = 4;
 
    //inputs
-   input [DW-1:0]  a;         //first operand
+   input [DW-1:0]  in;        //input operand
 
    //outputs
-   output [DW-1:0] out;       //out=abs(a)
+   output [DW-1:0] out;       //out = abs(in) (signed two's complement)
    output 	   overflow;  //high for max negative #
-   
+
+
+   assign out[DW-1:0] = in[DW-1] ? ~in[DW-1:0] + 1'b1 :
+			            in[DW-1:0];
+
+   assign overflow = in[DW-1] & ~(|in[DW-2:0]);
+
 endmodule // oh_abs
 
