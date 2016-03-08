e622aa1b333cfd684dfcaef65734eb61ba4bc5ef
Andreas Olofsson <andreas@adapteva.com>
2016-03-08 15:49:54 -0500

    Reorg

--- common/dv/build.sh
+++ common/dv/build.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/bin/bash 
+############################################################################
+# Icarus Verilog build script for OH! 
+# Requires $OH_HOME variable to be set
+############################################################################
 
-DV_FILE=$1
-DUT_FILE=$2
-CFG=$3
-LIBS=$OH_HOME/common/dv/libs.cmd
+DUT=$1
 
-iverilog -g2005 -DTARGET_SIM=1 $CFG $DV_FILE $DUT_FILE -f $LIBS -o dut.bin
-
-
-
+#Create directory of all links
+mkdir -p $OH_HOME/symlinks/hdl
+mkdir -p $OH_HOME/symlinks/dv
+pushd $OH_HOME/symlinks/hdl
+ln -s ../../*/hdl/*.v .
+cd ../dv
+ln -s ../../*/dv/*.v .
+popd
+exit
+#Build
+iverilog -g2005\
+ -DTARGET_SIM=1\
+ $DUT\
+ $OH_HOME/symlinks/dv/dv_top.v\
+ -y .\
+ -y $OH_HOME/symlinks/hdl\
+ -y $OH_HOME/symlinks/dv\
+ -I $OH_HOME/symlinks/hdl
+ -o dut.bin\
