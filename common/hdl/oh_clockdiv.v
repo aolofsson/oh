e900ecca2ac906c8b859c3c0a46d82150abcdc3c
Andreas Olofsson <andreas@adapteva.com>
2016-03-10 11:06:28 -0500

    Simplifying clockdiv -tested in spi block -more generic, simpler

--- common/hdl/oh_clockdiv.v
+++ common/hdl/oh_clockdiv.v
@@ -1,107 +1,86 @@
-// ###############################################################
-// # FUNCTION: Synchronous clock divider that divides by integer
-// # divcfg: 0 = div1
-// #         1 = div2
-// #         2 = div4
-// #         3 = div8
-// #         4 = div16
-// #         5 = div32
-// #         6 = div64
-// #         7 = div128
-// #       15:8= reserved   
-// ############################################################### 
+//#############################################################################
+//# Purpose: Simple clock divider (modulo 2)                                  #
+//#          clkdiv: 0-->divide by 1                                          #
+//#          clkdiv: 1-->divide by 2                                          #
+//#          clkdiv: 2-->divide by 4                                          #
+//#          clkdiv: 2-->divide by 8  etc..                                   #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see below)                                                 # 
+//#############################################################################
+
 module oh_clockdiv(/*AUTOARG*/
    // Outputs
-   clkout, clkout90,
+   period_match, phase_match, clkout,
    // Inputs
-   clk, en, nreset, divcfg
+   clk, nreset, en, clkdiv
    );
 
-   //signals 
-   input       clk;      // input clock
-   input       en;       // synchronous clock enable
-   input       nreset;   // async   
-   input [3:0] divcfg;   // divide factor (1-128)
-   output      clkout;   // divided clock phase aligned with clkin
-   output      clkout90; // clkout shifted by 90 deg
-
-   //regs
-   reg        clkout_reg;
-   reg        clkout90_reg;
-   reg [7:0]  counter;   
-   reg [7:0]  divcfg_dec;
-   reg [3:0]  divcfg_reg;
-   reg 	      clkout90_div2;
-   
-   wire       posedge_match;
-   wire       negedge_match;  
-    
-   // divider setting
-   always @ (divcfg_reg[3:0])
-     casez (divcfg_reg[3:0])
-       4'b0001 : divcfg_dec[7:0] = 8'b00000010;  // Divide by 2
-       4'b0010 : divcfg_dec[7:0] = 8'b00000100;  // Divide by 4
-       4'b0011 : divcfg_dec[7:0] = 8'b00001000;  // Divide by 8
-       4'b0100 : divcfg_dec[7:0] = 8'b00010000;  // Divide by 16
-       4'b0101 : divcfg_dec[7:0] = 8'b00100000;  // Divide by 32
-       4'b0110 : divcfg_dec[7:0] = 8'b01000000;  // Divide by 64
-       4'b0111 : divcfg_dec[7:0] = 8'b10000000;  // Divide by 128
-       default : divcfg_dec[7:0] = 8'b00000000;  // others (divide by 1)
-     endcase
-
-   // divcfg change detector
-   always @ (posedge clk or negedge nreset)
-     if(!nreset)
-       divcfg_reg[3:0]<='b0;   //set to bypass when  in reset (fast clocks)
-     else
-       divcfg_reg[3:0]<=divcfg[3:0];
-
-   assign cfg_reset = (|(divcfg_reg[3:0] ^ divcfg[3:0]));
-   
-   // synchronous edge counter
-   always @ (posedge clk or negedge nreset)
-     if(!nreset)
-       counter[7:0] <= 8'b00000001;
-     else if(posedge_match | cfg_reset)
-       counter[7:0] <= 8'b00000001;// Self resetting
-     else if (en)
-       counter[7:0] <= (counter[7:0] + 8'b00000001);
-   
-   assign posedge_match    = (counter[7:0]==divcfg_dec[7:0]);
-   assign negedge_match    = (counter[7:0]=={1'b0,divcfg_dec[7:1]}); 
-   assign posedge90_match  = (counter[7:0]=={2'b0,divcfg_dec[7:2]});
-   assign negedge90_match  = (counter[7:0]=={2'b0,divcfg_dec[7:2]}+{1'b0,divcfg_dec[7:1]}); 
+   parameter DW   = 8;          // divider counter width
+   parameter CW   = $clog2(DW); // config width
       
-   // clkout
-   always @ (posedge clk or negedge nreset)
-     if(!nreset)
-       clkout_reg <= 1'b0;   
-     else if(posedge_match)
-       clkout_reg <= 1'b1;
-     else if(negedge_match)
-       clkout_reg <= 1'b0;
- 
-   // divide by one special case
-   assign clkout  = (divcfg_reg[3:0]==4'b0000) ? clk : clkout_reg;
-
-   // clkout90
-   always @ (posedge clk or negedge nreset)
-     if(!nreset)
-       clkout90_reg <= 1'b0;   
-     else if(posedge90_match)
-       clkout90_reg <= 1'b1;
-     else if(negedge90_match)
-       clkout90_reg <= 1'b0;
-
-   // special div2 case, using negedge of clk to delay by 90 deg
-   always @ (negedge clk)
-     clkout90_div2 <= clkout_reg;
+   input          clk;          // main clock
+   input          nreset;       // async active low reset
+   input          en;           // counter enable
+   input [CW-1:0] clkdiv;       // counter width
    
-   // divide by one and two special cases
-   assign clkout90  = (divcfg_reg[3:0]==4'b0000) ? clk           : 
-		      (divcfg_reg[3:0]==4'b0001) ? clkout90_div2 :
-                             	    	           clkout90_reg;
+   output         period_match; // period match
+   output         phase_match;  // phase match
+   output 	  clkout;       // output clock
+   
+   reg [DW-1:0]   counter;      //free running counter!
+   reg 		  clkout;
+   
+   //baud counter
+   always @ (posedge clk or negedge nreset)
+     if(!nreset)
+       counter[7:0] <= 'b0;
+     else if(en)
+       if(period_match)
+	 counter[7:0] <= 'b0;
+       else
+	 counter[7:0] <= counter[7:0] + 1'b1;
+
+   assign period_match=(counter[DW-1:0]==((1<<clkdiv[CW-1:0])-1'b1));
+   assign phase_match =(counter[DW-1:0]==((1<<(clkdiv[CW-1:0])>> 1)-1'b1));
       
+   //clock generator
+   always @ (posedge clk or negedge nreset)
+     if(!nreset)
+       clkout <= 1'b0;      
+     else if(phase_match)
+       clkout <= 1'b1;
+     else if(period_match)
+       clkout <= 1'b0;
+
 endmodule // oh_clockdiv
+
+
+///////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)                                                     //
+//                                                                           //
+// Copyright (c) 2015-2016, Adapteva, Inc.                                   //
+//                                                                           //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software") //
+// to deal in the Software without restriction, including without limitation // 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
+//                                                                           //
+// The above copyright notice and this permission notice shall be included   // 
+// in all copies or substantial portions of the Software.                    //
+//                                                                           //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.    //
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY      //
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT //
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  //
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                //
+//                                                                           //  
+///////////////////////////////////////////////////////////////////////////////
+
+
 
     
