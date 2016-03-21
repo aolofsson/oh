d800325f50fa53b54cb4c6812b1866dd27c84a8a
Andreas Olofsson <andreas@adapteva.com>
2016-03-20 22:37:30 -0400

    Reformatting to use "N" as the main parameters -simpler..

--- mio/hdl/mrx_io.v
+++ mio/hdl/mrx_io.v
@@ -13,21 +13,21 @@ module mrx_io (/*AUTOARG*/
    //#####################################################################
 
    //parameters
-   parameter  MIOW  = 16;  
+   parameter  N  = 16;  
 
    //RESET
-   input               nreset;        // async active low reset
-   input 	       clk;           // clock for IO
+   input            nreset;        // async active low reset
+   input 	    clk;           // clock for IO
    //IO interface
-   input [MIOW-1:0]    rx_packet;     // data for IO
-   input 	       rx_access;     // access signal for IO
+   input [N-1:0]    rx_packet;     // data for IO
+   input 	    rx_access;     // access signal for IO
    
    //FIFO interface (core side)
-   output 	       io_access;     // fifo packet valid
-   output [2*MIOW-1:0] io_packet;     // fifo packet
+   output 	    io_access;     // fifo packet valid
+   output [2*N-1:0] io_packet;     // fifo packet
    
    //regs
-   reg 		       io_access;
+   reg 		    io_access;
    
    //########################################
    //# CLOCK, RESET
@@ -53,12 +53,12 @@ module mrx_io (/*AUTOARG*/
    //# DATA (DDR) 
    //########################################
    
-   oh_iddr #(.DW(MIOW))
-   data_iddr(.q1			(io_packet[MIOW-1:0]),
-	     .q2			(io_packet[2*MIOW-1:MIOW]),
+   oh_iddr #(.DW(N))
+   data_iddr(.q1			(io_packet[N-1:0]),
+	     .q2			(io_packet[2*N-1:N]),
 	     .clk			(clk),
 	     .ce			(rx_access),
-	     .din			(rx_packet[MIOW-1:0])
+	     .din			(rx_packet[N-1:0])
 	     );
    
 endmodule // mrx_io
