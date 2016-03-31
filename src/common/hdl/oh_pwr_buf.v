9db6638d6808a1d958076cc66cb13f000633724d
Andreas Olofsson <andreas@adapteva.com>
2016-03-31 19:18:38 -0400

    Cell renaming -Getting too many cells, loose grouping helps me find cells.(not formal) -Clear and major "blocks" should have their own folder structure

---
+++ src/common/hdl/oh_pwr_buf.v
@@ -0,0 +1,53 @@
+//#############################################################################
+//# Function: Buffer that propagates "X" if power supply is invalid           #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see below)                                                 # 
+//#############################################################################
+
+module oh_pwr_buf (/*AUTOARG*/
+   // Outputs
+   out,
+   // Inputs
+   vdd, vss, in
+   );
+
+   parameter DW=1;         // width of macro
+
+   input           vdd;    // supply (set to 1 if valid)
+   input           vss;    // ground (set to 0 if valid)
+   input [DW-1:0]  in;     // input signal
+   output [DW-1:0] out;    // buffered output signal
+   
+`ifdef TARGET_SIM   
+   assign out = ((vdd===1'b1) && (vss===1'b0)) ? in : 1'bX;
+`else
+   assign out = in;
+`endif
+   
+endmodule // oh_buf
+
+///////////////////////////////////////////////////////////////////////////////
+// The MIT License (MIT)                                                     //
+//                                                                           //
+// Copyright (c) 2015-2016, Adapteva, Inc.                                   //
+//                                                                           //
+// Permission is hereby granted, free of charge, to any person obtaining a   //
+// copy of this software and associated documentation files (the "Software") //
+// to deal in the Software without restriction, including without limitation // 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
+// and/or sell copies of the Software, and to permit persons to whom the     //
+// Software is furnished to do so, subject to the following conditions:      //
+//                                                                           //
+// The above copyright notice and this permission notice shall be included   // 
+// in all copies or substantial portions of the Software.                    //
+//                                                                           //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.    //
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY      //
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT //
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  //
+// THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                //
+//                                                                           // 
+///////////////////////////////////////////////////////////////////////////////
