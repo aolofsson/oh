a71caf2f42971532f36f0f4fa0ff6527b2804dea
Andreas Olofsson <andreas@adapteva.com>
2016-04-04 09:00:38 -0400

    Removing confusing names on signals - The c2io, io2c should never be inside the module! - Use in/out to clarify direction in interface modules - The signal direction is indicated at the connectoin level (wrapper)...final decision....

--- src/emesh/hdl/emesh_if.v
+++ src/emesh/hdl/emesh_if.v
@@ -1,114 +1,98 @@
 //WARNING: Pass through logic
 module emesh_if (/*AUTOARG*/
    // Outputs
-   c2e_cmesh_wait_out, e2c_cmesh_access_out, e2c_cmesh_packet_out,
-   c2e_rmesh_wait_out, e2c_rmesh_access_out, e2c_rmesh_packet_out,
-   c2e_xmesh_wait_out, e2c_xmesh_access_out, e2c_xmesh_packet_out,
-   e2c_emesh_wait_out, c2e_emesh_access_out, c2e_emesh_packet_out,
+   cmesh_wait_out, cmesh_access_out, cmesh_packet_out, rmesh_wait_out,
+   rmesh_access_out, rmesh_packet_out, xmesh_wait_out,
+   xmesh_access_out, xmesh_packet_out, emesh_wait_out,
+   emesh_access_out, emesh_packet_out,
    // Inputs
-   c2e_cmesh_access_in, c2e_cmesh_packet_in, e2c_cmesh_wait_in,
-   c2e_rmesh_access_in, c2e_rmesh_packet_in, e2c_rmesh_wait_in,
-   c2e_xmesh_access_in, c2e_xmesh_packet_in, e2c_xmesh_wait_in,
-   e2c_emesh_access_in, e2c_emesh_packet_in, c2e_emesh_wait_in
+   cmesh_access_in, cmesh_packet_in, cmesh_wait_in, rmesh_access_in,
+   rmesh_packet_in, rmesh_wait_in, xmesh_access_in, xmesh_packet_in,
+   xmesh_wait_in, emesh_access_in, emesh_packet_in, emesh_wait_in
    );
 
    parameter PW = 99;  
 
    //##Cmesh##    
-   //core-->io
-   input 	  c2e_cmesh_access_in;
-   input [PW-1:0] c2e_cmesh_packet_in;
-   output 	  c2e_cmesh_wait_out;
-   //io-->core
-   output 	   e2c_cmesh_access_out;
-   output [PW-1:0] e2c_cmesh_packet_out;
-   input 	   e2c_cmesh_wait_in;
+   input 	   cmesh_access_in;
+   input [PW-1:0]  cmesh_packet_in;
+   output 	   cmesh_wait_out;
+   output 	   cmesh_access_out;
+   output [PW-1:0] cmesh_packet_out;
+   input 	   cmesh_wait_in;
       
    //##Rmesh## 
-   //core-->io
-   input 	   c2e_rmesh_access_in;
-   input [PW-1:0]  c2e_rmesh_packet_in;
-   output 	   c2e_rmesh_wait_out;
-   //io-->core
-   output 	   e2c_rmesh_access_out;
-   output [PW-1:0] e2c_rmesh_packet_out;
-   input 	   e2c_rmesh_wait_in;
+   input 	   rmesh_access_in;
+   input [PW-1:0]  rmesh_packet_in;
+   output 	   rmesh_wait_out;
+   output 	   rmesh_access_out;
+   output [PW-1:0] rmesh_packet_out;
+   input 	   rmesh_wait_in;
    
    //##Xmesh## 
-
-   //core-->io
-   input 	   c2e_xmesh_access_in;
-   input [PW-1:0]  c2e_xmesh_packet_in;
-   output 	   c2e_xmesh_wait_out;
-   
-   //io-->core
-   output 	   e2c_xmesh_access_out;
-   output [PW-1:0] e2c_xmesh_packet_out;
-   input 	   e2c_xmesh_wait_in;
+   input 	   xmesh_access_in;
+   input [PW-1:0]  xmesh_packet_in;
+   output 	   xmesh_wait_out;  
+   output 	   xmesh_access_out;
+   output [PW-1:0] xmesh_packet_out;
+   input 	   xmesh_wait_in;
    
    //##Emesh##
-
-   //io-->core
-   input 	   e2c_emesh_access_in;
-   input [PW-1:0]  e2c_emesh_packet_in;
-   output 	   e2c_emesh_wait_out;
+   input 	   emesh_access_in;
+   input [PW-1:0]  emesh_packet_in;
+   output 	   emesh_wait_out;
    
    //core-->io
-   output 	   c2e_emesh_access_out;
-   output [PW-1:0] c2e_emesh_packet_out;
-   input 	   c2e_emesh_wait_in;
-   
-   
-   //local wires
-   wire 	   e2c_emesh_write;
-  
+   output 	   emesh_access_out;
+   output [PW-1:0] emesh_packet_out;
+   input 	   emesh_wait_in;
+      
    //#####################################################
-   //# EXTERNAL 2 CORE (E2C)
+   //# EMESH-->(RMESH/XMESH/CMESH)
    //#####################################################
       
-   //Access signals
-   assign e2c_cmesh_access_out = e2c_emesh_access_in & e2c_emesh_packet_in[0];
-   
-   assign e2c_rmesh_access_out = e2c_emesh_access_in & ~e2c_emesh_packet_in[0];
+   assign cmesh_access_out = emesh_access_in & emesh_packet_in[0];
 
-   assign e2c_xmesh_access_out = 1'b0;
+   assign rmesh_access_out = emesh_access_in & ~emesh_packet_in[0];
+
+   //Don't drive on xmesh for now
+   assign xmesh_access_out = 1'b0;
       
    //Distribute emesh to xmesh,cmesh, rmesh
-   assign e2c_cmesh_packet_out[PW-1:0] = e2c_emesh_packet_in[PW-1:0];	 
-   assign e2c_rmesh_packet_out[PW-1:0] = e2c_emesh_packet_in[PW-1:0];	 
-   assign e2c_xmesh_packet_out[PW-1:0] = e2c_emesh_packet_in[PW-1:0];
+   assign cmesh_packet_out[PW-1:0] = emesh_packet_in[PW-1:0];	 
+   assign rmesh_packet_out[PW-1:0] = emesh_packet_in[PW-1:0];	 
+   assign xmesh_packet_out[PW-1:0] = emesh_packet_in[PW-1:0];
 
-   //Pushback from core logic
-   assign e2c_emesh_wait_out = e2c_cmesh_wait_in |
-			       e2c_rmesh_wait_in |
-			       e2c_xmesh_wait_in;
+
+   assign emesh_wait_out = cmesh_wait_in |
+			   rmesh_wait_in |
+			   xmesh_wait_in;
   	 	 
    //#####################################################
-   //# CORE 2 EXTERNAL (C2E) 
+   //# (RMESH/XMESH/CMESH)-->EMESH
    //#####################################################
 
-   //Access aggregration 
-   assign c2e_emesh_access_out = c2e_cmesh_access_in |
-				 c2e_rmesh_access_in |
-				 c2e_xmesh_access_in;
-   
-   //Simple priority decode (watch out for deadlock!)
-   assign c2e_emesh_packet_out[PW-1:0] = c2e_cmesh_access_in ? c2e_cmesh_packet_in[PW-1:0] :
-					 c2e_rmesh_access_in ? c2e_rmesh_packet_in[PW-1:0] :
-					                       c2e_xmesh_packet_in[PW-1:0];
-   
-				
-   //Wait pushback (for all, don't want loopback paths)
-
-   assign c2e_cmesh_wait_out = c2e_emesh_wait_in;
+   assign emesh_access_out = cmesh_access_in |
+			     rmesh_access_in |
+			     xmesh_access_in;
    
 
-   assign c2e_rmesh_wait_out = c2e_emesh_wait_in;
+   //TODO: Make round robin?? (Fancify)
+   assign emesh_packet_out[PW-1:0] = cmesh_access_in ? cmesh_packet_in[PW-1:0] :
+				     rmesh_access_in ? rmesh_packet_in[PW-1:0] :
+				                       xmesh_packet_in[PW-1:0];
+   
+   assign cmesh_wait_out = (cmesh_access_in & emesh_wait_in);
+   
+
+   assign rmesh_wait_out = rmesh_access_in & 
+			   (emesh_wait_in | cmesh_access_in);
 				
 
-   assign c2e_xmesh_wait_out = c2e_emesh_wait_in;
+   assign xmesh_wait_out = xmesh_access_in & 
+			   (emesh_wait_in | cmesh_access_in | rmesh_access_in);
    
-   				     
+				     
 endmodule // emesh_if
 
-
+											 
