5b8328826e28b536083cce3ad09775990034ff02
Andreas Olofsson <andreas@adapteva.com>
2016-04-11 11:59:18 -0400

    Making rsync interface scalar - Who ever uses a vectorized reset.... (if you do, use generate...)

--- src/common/hdl/oh_rsync.v
+++ src/common/hdl/oh_rsync.v
@@ -1,59 +1,26 @@
-/* A synchronization circuit for reset signals
- * Async reset assertion and sync reset deassertion on otput
- */ 
-module oh_rsync (/*AUTOARG*/
-   // Outputs
-   nrst_out,
-   // Inputs
-   clk, nrst_in
-   );
+//#############################################################################
+//# Function: Reset synchronizer (async assert, sync deassert)                #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//#############################################################################
 
-   parameter PS   = 2; //number of sync pipeline stages
-   parameter DW   = 1; //number of bits to synchronize
+module oh_rsync #(parameter PS =2)  // number of sync stages
+   (
+    input  clk,
+    input  nrst_in,
+    output nrst_out
+    );
+  
+   reg [PS-1:0] sync_pipe;
    
-   input           clk;
-   input [DW-1:0]  nrst_in;
-   output [DW-1:0] nrst_out;
+   always @ (posedge clk or negedge nrst_in)		 
+     if(!nrst_in)
+       sync_pipe[PS-1:0] <= 1'b0;
+     else
+       sync_pipe[PS-1:0] <= {sync_pipe[PS-2:0],1'b1};	      
    
-`ifdef TARGET_SIM
-   reg [DW-1:0]    sync_pipe[PS-1:0];
-`else
-   (* ASYNC_REG = "TRUE"  *) (* DONT_TOUCH =  "TRUE" *) reg [DW-1:0]    sync_pipe[PS-1:0];   
-`endif
-   
-   genvar 	i;
-   genvar 	j;   
-
-   //TODO: simplify logic
-   generate          
-      for(i=0;i<PS;i=i+1)
-	begin : stage
-	if(i==0)
-	  begin : first_stage
-	     for(j=0;j<DW;j=j+1)		 
-	       begin : first_stage_in
-		  always @ (posedge clk or negedge nrst_in[j])		 
-		    if(!nrst_in[j])
-		      sync_pipe[0][j] <= 1'b0;
-		    else
-		      sync_pipe[0][j] <= 1'b1;	     
-	       end
-	  end
-	else
-	  begin : second_stage
-	     for(j=0;j<DW;j=j+1)		 
-	       begin : second_stage_in
-		  always @ (posedge clk or negedge nrst_in[j])		 
-		    if(!nrst_in[j])
-		      sync_pipe[i][j] <= 1'b0;
-		    else
-		      sync_pipe[i][j] <=  sync_pipe[i-1][j]; 
-	       end
-	  end	  	 
-	end
-   endgenerate
-   
-   assign nrst_out[DW-1:0] = sync_pipe[PS-1][DW-1:0];
+   assign nrst_out = sync_pipe[PS-1];
    		    	
 endmodule // oh_rsync
 
