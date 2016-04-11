01a74a3db9ad494db297efde3e9ae2ca0ab537b3
Andreas Olofsson <andreas@adapteva.com>
2016-04-11 10:30:02 -0400

    Removing ce from oddr - Better to turn off clock if you want to save power - The CE was actually increasing the power by N x by making every data signal a clock :-)

--- src/common/hdl/oh_oddr.v
+++ src/common/hdl/oh_oddr.v
@@ -1,29 +1,19 @@
-//##################################################################
-//# ***DUAL DATA RATE OUTPUT***
-//#
-//# * Equivalent to "SAME_EDGE" for xilinx
-//# * din1/din2 presented together on rising edge of clk
-//# * din1 follows rising edge
-//# * din2 follows falling edge
-//##################################################################
+//#############################################################################
+//# Function: Dual data rate output buffer                                    #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see LICENSE file in this repository)                       # 
+//#############################################################################
 
-module oh_oddr (/*AUTOARG*/
-   // Outputs
-   out,
-   // Inputs
-   clk, ce, din1, din2
-   );
-  
-   //parameters
-   parameter DW            = 32;
+module oh_oddr #(parameter DW      = 1 // width of data inputs
+		 )
+   (
+    input 	    clk, // clock input
+    input [DW-1:0]  din1, // data input1
+    input [DW-1:0]  din2, // data input2
+    output [DW-1:0] out   // ddr output
+    );
    
-   //signals
-   input           clk;   // clock input
-   input           ce;    // clock enable input
-   input [DW-1:0]  din1;  // data input1
-   input [DW-1:0]  din2;  // data input2
-   output [DW-1:0] out;   // ddr output
-
    //regs("sl"=stable low, "sh"=stable high)
    reg [DW-1:0]    q1_sl;   
    reg [DW-1:0]    q2_sl;
@@ -31,11 +21,10 @@ module oh_oddr (/*AUTOARG*/
       
    //Generate different logic based on parameters
    always @ (posedge clk)
-     if (ce)
-       begin
-	  q1_sl[DW-1:0] <= din1[DW-1:0];
-	  q2_sl[DW-1:0] <= din2[DW-1:0];
-       end
+     begin
+	q1_sl[DW-1:0] <= din1[DW-1:0];
+	q2_sl[DW-1:0] <= din2[DW-1:0];
+     end
    
    always @ (negedge clk)
      q2_sh[DW-1:0] <= q2_sl[DW-1:0];
