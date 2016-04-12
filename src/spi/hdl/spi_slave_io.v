30555c53753585fb9f9640715d7d8a86b4c720dd
Andreas Olofsson <andreas@adapteva.com>
2016-04-12 09:42:52 -0400

    Adding nreset to command reg - Not 100% sure on this. Ideally I think you would want the ss signal to be the "reset" of all this logic, but the command register gets too tricky. - Is there an issue in having a async active low reset pin on the this interface. Considering that this is only a problem for the remote fetch logic (which will definitely have a nreset signal) I think this is ok...

--- src/spi/hdl/spi_slave_io.v
+++ src/spi/hdl/spi_slave_io.v
@@ -93,8 +93,10 @@ module spi_slave_io(/*AUTOARG*/
         
    // command/address register
    // auto increment for every byte
-   always @ (negedge sclk)
-     if((spi_state[1:0]==`SPI_CMD) & byte_done)
+   always @ (negedge sclk or negedge nreset)
+     if(!nreset)
+       command_reg[7:0] <= 'b0;
+     else if((spi_state[1:0]==`SPI_CMD) & byte_done)
        command_reg[7:0] <= rx_data[7:0];
      else if(byte_done)
        command_reg[7:0] <= {command_reg[7:6],
@@ -174,6 +176,7 @@ module spi_slave_io(/*AUTOARG*/
    
    //sync the ss to free running clk
    //look for rising edge
+
    oh_dsync dsync (.dout (ss_sync),
 		   .clk  (clk),
 		   .din  (ss));
@@ -196,28 +199,3 @@ endmodule // spi_slave_io
 // Local Variables:
 // verilog-library-directories:("." "../../common/hdl")
 // End:
-
-///////////////////////////////////////////////////////////////////////////////
-// The MIT License (MIT)                                                     //
-//                                                                           //
-// Copyright (c) 2015-2016, Adapteva, Inc.                                   //
-//                                                                           //
-// Permission is hereby granted, free of charge, to any person obtaining a   //
-// copy of this software and associated documentation files (the "Software") //
-// to deal in the Software without restriction, including without limitation // 
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,  //
-// and/or sell copies of the Software, and to permit persons to whom the     //
-// Software is furnished to do so, subject to the following conditions:      //
-//                                                                           //
-// The above copyright notice and this permission notice shall be included   // 
-// in all copies or substantial portions of the Software.                    //
-//                                                                           //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   //
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                //
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.    //
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY      //
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT //
-// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR  //
-// THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                //
-//                                                                           //  
-///////////////////////////////////////////////////////////////////////////////
