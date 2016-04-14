42c7f4ed0dd44ce922167f98a04ec00f6a6fdbcb
Andreas Olofsson <andreas@adapteva.com>
2016-04-13 20:48:37 -0400

    Improving dsync - adding ability to drive random delay values on each bit in multibit sync - adding async reset (need was found during integration testing)

--- src/common/hdl/oh_dsync.v
+++ src/common/hdl/oh_dsync.v
@@ -9,25 +9,42 @@ module oh_dsync  #(parameter DW = 1, // width of data
 		   parameter PS = 3  // mnumber of sync stages
 		   )
    (
-    input 	    clk, // clock
-    input [DW-1:0]  din, // input data
-    output [DW-1:0] dout  // synchronized data
+    input 	    clk,    // clock
+    input 	    nreset, // clock
+    input [DW-1:0]  din,    // input data
+    output [DW-1:0] dout    // synchronized data
     );
    
-   reg [DW-1:0]     sync_pipe[PS-1:0];
+   reg [DW-1:0]     sync_pipe[PS:0]; //extra cycle for DV
    
-   genvar 	   i;
+
+   // variable length synchronizer pipe
+   genvar 	    i;
    generate          
-      for(i=0;i<PS;i=i+1)
+      for(i=0;i<(PS+1);i=i+1)
 	if(i==0)
-	  always @ (posedge clk)
-	    sync_pipe[0][DW-1:0] <= din[DW-1:0];	     
+	  always @ (posedge clk or negedge nreset)
+	    if(!nreset)
+	      sync_pipe[0][DW-1:0] <= 'b0;	     
+	    else
+	      sync_pipe[0][DW-1:0] <= din[DW-1:0];	     
 	else
-	  always @ (posedge clk )
-	    sync_pipe[i][DW-1:0] <= sync_pipe[i-1][DW-1:0];	     
+	  always @ (posedge clk or negedge nreset)
+	    if(!nreset)
+	      sync_pipe[i][DW-1:0] <= 'b0;	   
+	    else
+	      sync_pipe[i][DW-1:0] <= sync_pipe[i-1][DW-1:0];	     
    endgenerate
    
-   assign dout[DW-1:0] = sync_pipe[PS-1][DW-1:0];
+`ifdef TARGET_SIM
+   // randomize sync delay based on value in per bit delay register
+   // delay to be forced from testbench
+   reg [DW-1:0]     delay = 0;
+   assign dout[DW-1:0] = (delay[DW-1:0]  & sync_pipe[PS][DW-1:0]) |  //extra cycle
+			 (~delay[DW-1:0] & sync_pipe[PS-1][DW-1:0]); //default
+`else
+   assign dout[DW-1:0] = sync_pipe[PS-1][DW-1:0];   
+`endif
     
 endmodule // oh_dsync
 
