c78c24899fe74db88a19b1cee5cca543c54722fd
Andreas Olofsson <andreas@adapteva.com>
2016-05-15 15:45:05 -0400

    Updating emesh packet mappers - modified 64b interface - adding some other address widths/types - making AW/PW orthogonal for added flexibility, this means wheneve these blocks are used, you have to specify AW/PW - making 32/104 the default for backwards compatibility - changing to v2005 interface - adding license

--- src/emesh/hdl/emesh2packet.v
+++ src/emesh/hdl/emesh2packet.v
@@ -1,71 +1,67 @@
-/*
- * ---- 32-BIT ADDRESS ----
- * [1]       write bit
- * [2:1]     datamode
- * [6:3]     ctrlmode
- * [7]       RESERVED
- * [39:8]    f0 = dstaddr(lo)
- * [71:40]   f1 = data (lo)
- * [103:72]  f2 = srcaddr(lo) /  data (hi)
- * 
- * ---- 64-BIT ADDRESS ----
- * [0]       write bit
- * [2:1]     datamode
- * [7:3]     ctrlmode
- * [39:8]    f0 = dstaddr(lo)
- * [71:40]   f1 = D0
- * [103:72]  f2 = D1 | srcaddr(lo)
- * [135:104] f3 = D2 | srcaddr(hi)
- * [167:136] f4 = D3 | dstaddr(hi)
- * 
- */
-module emesh2packet(/*AUTOARG*/
-   // Outputs
-   packet_out,
-   // Inputs
-   write_out, datamode_out, ctrlmode_out, dstaddr_out, data_out,
-   srcaddr_out
-   );
-   parameter AW   = 32;   
-   parameter PW   = 2*AW+40; 
+//#############################################################################
+//# Function: Maps Emesh Signals to Packet                                    #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//#############################################################################
+module emesh2packet #(parameter AW = 32,   // address width 
+		      parameter PW = 104)  // packet width
+   (
+    //Emesh signal bundle
+    input 	    write_out, 
+    input [1:0]     datamode_out,
+    input [4:0]     ctrlmode_out,
+    input [AW-1:0]  dstaddr_out,
+    input [AW-1:0]  data_out, 
+    input [AW-1:0]  srcaddr_out, 
+    //Output packet
+    output [PW-1:0] packet_out
+    );
    
-   //Emesh signal bundle
-   input 	    write_out;   
-   input [1:0] 	    datamode_out;
-   input [4:0] 	    ctrlmode_out;
-   input [AW-1:0]   dstaddr_out;
-   input [AW-1:0]   data_out;   
-   input [AW-1:0]   srcaddr_out;   
+   // ---- FORMAT -----
+   //
+   // [0]  =write bit
+   // [2:1]=datamode
+   // [7:3]=ctrlmode
+   // [39:8]=dstaddr(lo)
+   //
+   // ---- 32-BIT ADDRESS ----
+   // [71:40]   data (lo)   | xxxx
+   // [103:72]  srcaddr(lo) | data (hi)
+   //
+   // ---- 64-BIT ADDRESS ----
+   // [71:40]   D0 | srcaddr(hi)
+   // [103:72]  D1 | srcaddr(lo)
+   // [135:104] dstaddr(hi)
    
-   //Output packet
-   output [PW-1:0]  packet_out;
-
    assign packet_out[0]       = write_out;   
    assign packet_out[2:1]     = datamode_out[1:0];
    assign packet_out[7:3]     = ctrlmode_out[4:0];
      
    generate   
-      if(AW==64)
-	begin : packet64
+     if(PW==136)
+	begin : p136
 	   assign packet_out[39:8]    = dstaddr_out[31:0];
-	   assign packet_out[71:40]   = data_out[31:0];
-	   assign packet_out[103:72]  = srcaddr_out[31:0];   
-	   assign packet_out[135:104] = srcaddr_out[63:32];
-	   assign packet_out[167:136] = dstaddr_out[63:32];
+	   assign packet_out[71:40]   = data_out[31:0];    // | srcaddr_out[63:32]
+	   assign packet_out[103:72]  = srcaddr_out[31:0]; // (data_out[63:32])   
+	   assign packet_out[135:104] = dstaddr_out[63:32];
 	end
-      else if(AW==32)
-	begin : packet32
+      else if(PW==104)
+	begin : p104
 	   assign packet_out[39:8]    = dstaddr_out[31:0];
 	   assign packet_out[71:40]   = data_out[31:0];
 	   assign packet_out[103:72]  = srcaddr_out[31:0];
 	end
-      else if(AW==16)
-	begin : packet16
+      else if(PW==72)
+	begin : p72
+	   assign packet_out[39:8]    = dstaddr_out[31:0];
+	   assign packet_out[71:40]   = data_out[31:0];
+	end
+      else if(PW==40)
+	begin : p40
 	   assign packet_out[23:8]    = dstaddr_out[15:0];
 	   assign packet_out[39:24]   = data_out[15:0];
-	   assign packet_out[55:40]   = srcaddr_out[15:0];
 	end
-     
    endgenerate
    
 endmodule // emesh2packet
--- src/emesh/hdl/packet2emesh.v
+++ src/emesh/hdl/packet2emesh.v
@@ -1,49 +1,42 @@
-/*
- * ---- 32-BIT ADDRESS ----
- * [1]       write bit
- * [2:1]     datamode
- * [6:3]     ctrlmode
- * [7]       RESERVED
- * [39:8]    f0 = dstaddr(lo)
- * [71:40]   f1 = data (lo)
- * [103:72]  f2 = srcaddr(lo) /  data (hi)
- * 
- * ---- 64-BIT ADDRESS ----
- * [0]       write bit
- * [2:1]     datamode
- * [7:3]     ctrlmode
- * [39:8]    f0 = dstaddr(lo)
- * [71:40]   f1 = D0
- * [103:72]  f2 = D1 | srcaddr(lo)
- * [135:104] f3 = D2 | srcaddr(hi)
- * [167:136] f4 = D3 | dstaddr(hi)
- * 
- */
-module packet2emesh(/*AUTOARG*/
-   // Outputs
-   write_in, datamode_in, ctrlmode_in, dstaddr_in, srcaddr_in,
-   data_in,
-   // Inputs
-   packet_in
-   );
-
-   parameter AW     = 32;   
-   parameter PW     = (2*AW+40); 
-
-   //Input packet
-   input [PW-1:0]   packet_in;
-
-   //Emesh signal bundle 
-   output 	     write_in;
-   output [1:0]      datamode_in;
-   output [4:0]      ctrlmode_in;
-   output [AW-1:0]   dstaddr_in;
-   output [AW-1:0]   srcaddr_in;
-   output [AW-1:0]   data_in;
+//#############################################################################
+//# Function: Maps Packet to Emesh Signals                                    #
+//#############################################################################
+//# Author:   Andreas Olofsson                                                #
+//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//#############################################################################
+module packet2emesh #(parameter AW = 32,   // address width 
+		      parameter PW = 104)  // packet width
+   (
+    //Input packet
+    input [PW-1:0]  packet_in,
+    //Emesh signal bundle 
+    output 	    write_in,   // write signal
+    output [1:0]    datamode_in,// datasize
+    output [4:0]    ctrlmode_in,// ctrlmode
+    output [AW-1:0] dstaddr_in, // read/write address
+    output [AW-1:0] srcaddr_in, // return address for reads
+    output [AW-1:0] data_in // data
+    );
       
+   // ---- FORMAT -----
+   //
+   // [0]  =write bit
+   // [2:1]=datamode
+   // [7:3]=ctrlmode
+   // [39:8]=dstaddr(lo)
+   //
+   // ---- 32-BIT ADDRESS ----
+   // [71:40]   data (lo)   | xxxx
+   // [103:72]  srcaddr(lo) | data (hi)
+   //
+   // ---- 64-BIT ADDRESS ----
+   // [71:40]   D0 | srcaddr(hi)
+   // [103:72]  D1 | srcaddr(lo)
+   // [135:104] dstaddr(hi)
+   
    generate
-      if(AW==32)
-	begin : packet32
+      if(PW==104)
+	begin : p104
 	   assign write_in           = packet_in[0];   
 	   assign datamode_in[1:0]   = packet_in[2:1];   
 	   assign ctrlmode_in[4:0]   = {1'b0,packet_in[6:3]};   
@@ -51,14 +44,30 @@ module packet2emesh(/*AUTOARG*/
 	   assign srcaddr_in[31:0]   = packet_in[103:72];  
 	   assign data_in[31:0]      = packet_in[71:40]; 
 	end
-      else if(AW==64)
-	begin : packet64
+      else if(PW==136)
+	begin : p136
 	   assign write_in           = packet_in[0];
 	   assign datamode_in[1:0]   = packet_in[2:1];  
 	   assign ctrlmode_in[4:0]   = packet_in[7:3];
-	   assign dstaddr_in[63:0]   = {packet_in[167:135],packet_in[39:8]}; 
-	   assign srcaddr_in[63:0]   = packet_in[135:72];
+	   assign dstaddr_in[63:0]   = {packet_in[135:104],packet_in[39:8]}; 
+	   assign srcaddr_in[63:0]   = {packet_in[71:40],packet_in[135:72]};
 	   assign data_in[63:0]      = packet_in[103:40];
+	end
+      else if(PW==72)
+	begin : p72
+	   assign write_in           = packet_in[0];
+	   assign datamode_in[1:0]   = packet_in[2:1];  
+	   assign ctrlmode_in[4:0]   = packet_in[7:3];
+	   assign dstaddr_in[31:0]   = packet_in[39:8]; 
+	   assign data_in[31:0]      = packet_in[71:40];
+	end
+      else if(PW==40)
+	begin : p40
+	   assign write_in           = packet_in[0];
+	   assign datamode_in[1:0]   = packet_in[2:1];  
+	   assign ctrlmode_in[4:0]   = packet_in[7:3];
+	   assign dstaddr_in[15:0]   = packet_in[23:8]; 
+	   assign data_in[15:0]      = packet_in[39:24];
 	end
       else
 	begin : unknown
