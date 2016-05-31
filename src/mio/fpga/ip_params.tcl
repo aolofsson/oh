3652a123f0bad3e51fab3b5f797ccbdb7c069a8b
Ola Jeppsson <ola@adapteva.com>
2016-05-31 10:11:32 +0200

    mio: Add FPGA project
    Master AXI interface not connected.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

---
+++ src/mio/fpga/ip_params.tcl
@@ -0,0 +1,25 @@
+# NOTE: See UG1118 for more information
+
+set design parallella_mio
+set projdir ./
+set root "../.."
+set partname "xc7z020clg400-1"
+
+set hdl_files [list \
+	           $root/mio/hdl \
+		   $root/common/hdl/ \
+		   $root/emesh/hdl \
+		   $root/emmu/hdl \
+		   $root/axi/hdl \
+		   $root/emailbox/hdl \
+		   $root/edma/hdl \
+	           $root/elink/hdl \
+	           $root/parallella/hdl \
+		  ]
+
+set ip_files   [list \
+		    $root/xilibs/ip/fifo_async_104x32.xci \
+		   ]
+
+set constraints_files []
+
---
+++ src/mio/fpga/system_params.tcl
@@ -0,0 +1,32 @@
+# NOTE: See UG1118 for more information
+
+#########################################
+# VARIABLES
+#########################################
+set design axi_mio
+set projdir ./
+set root "../.."
+set partname "xc7z020clg400-1"
+
+set hdl_files [list \
+	           $root/mio/hdl \
+		   $root/common/hdl/ \
+		   $root/emesh/hdl \
+		   $root/emmu/hdl \
+		   $root/axi/hdl \
+		   $root/emailbox/hdl \
+		   $root/edma/hdl \
+	           $root/elink/hdl \
+	           $root/parallella/hdl \
+		  ]
+
+set ip_files   [list \
+		    $root/xilibs/ip/fifo_async_104x32.xci \
+		   ]
+
+set constraints_files [list \
+	../../parallella/fpga/parallella_io.xdc \
+	../../parallella/fpga/parallella_7020_io.xdc \
+	./axi_mio_timing.xdc \
+	]
+
