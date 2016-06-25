2623d2239e2c026c025db8e7c01f99de2447425c
Andreas Olofsson <andreas@adapteva.com>
2016-06-24 20:34:32 -0400

    Adding asiclib cell for lat0

--- src/common/hdl/oh_lat0.v
+++ src/common/hdl/oh_lat0.v
@@ -5,16 +5,31 @@
 //# License:  MIT (see LICENSE file in OH! repository)                        # 
 //#############################################################################
 
-module oh_lat0 #(parameter DW = 1) // data width
+module oh_lat0 #(parameter DW = 1,            // data width
+		 parameter ASIC  = `CFG_ASIC  // use ASIC lib
+		 ) 
    ( input 	     clk, // clk, latch when clk=0
-     input [DW-1:0] 	 in, // input data
-     output reg [DW-1:0] out  // output data (stable/latched when clk=1)
+     input [DW-1:0]  in, // input data
+     output [DW-1:0] out  // output data (stable/latched when clk=1)
      );
    
-   always @ (clk or in)
-     if (!clk)
-       out[DW-1:0] <= in[DW-1:0];
-
+   generate
+      if(ASIC)
+	begin : g0
+	   asic_lat0 i_lat [DW-1:0] (.clk(clk),
+				     .in(in[DW-1:0]),
+				     .out(out[DW-1:0]));
+	end
+      else
+	begin
+	   reg [DW-1:0] out_reg;	   
+	   always @ (clk or in)
+	     if (!clk)
+	       out_reg[DW-1:0] <= in[DW-1:0];
+	   assign out[DW-1:0] = out_reg[DW-1:0];	   
+	end // else: !if(ASIC)
+   endgenerate
+   
 endmodule // oh_lat0
 
 
