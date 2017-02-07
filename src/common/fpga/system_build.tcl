23c2f8b3837207cc43ac5ebcdebead7ba19e2d9a
Ola Jeppsson <ola@adapteva.com>
2017-02-07 18:42:20 +0100

    fpga/system_build.tcl: Tweak implementation optimization settings
    This is what ADI HDL uses. I trust that they know what they're doing.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- src/common/fpga/system_build.tcl
+++ src/common/fpga/system_build.tcl
@@ -39,6 +39,9 @@ report_timing_summary -file timing_synth.log
 ###########################################################
 # PLACE AND ROUTE
 ###########################################################
+set_property STEPS.PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
+set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
+set_property STRATEGY "Performance_Explore" [get_runs impl_1]
 launch_runs impl_1
 wait_on_run impl_1
 open_run impl_1
