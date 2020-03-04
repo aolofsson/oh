4f0f81997e0f886f403f01a189dc48de272525d7
Andreas.Olofsson <andreas@adapteva.com>
2020-03-04 18:36:38 -0500

    Fix datagate bug! -Was not turned on at all! Alwats on

--- common/hdl/oh_datagate.v
+++ common/hdl/oh_datagate.v
@@ -4,9 +4,10 @@
 //# Author:   Andreas Olofsson                                                #
 //# License:  MIT (see LICENSE file in OH! repository)                        # 
 //#############################################################################
-
-module oh_datagate #(parameter DW   = 32, // width of data inputs
-		     parameter PS   = 3   // min quiet time before shutdown
+//
+  
+module oh_datagate #(parameter DW = 32, // width of data inputs
+		     parameter N  = 3   // min quiet time before shutdown
 		     )
    ( 
      input 	     clk, // clock
@@ -14,16 +15,15 @@ module oh_datagate #(parameter DW   = 32, // width of data inputs
      input [DW-1:0]  din, // data input
      output [DW-1:0] dout // data output    
      );
-   
- 	  	 
-   reg [PS-1:0]    enable_pipe;   
-   wire 	   enable;
+  	  	 
+   reg [N-1:0]    enable_pipe;   
    
    always @ (posedge clk)
      enable_pipe[PS-1:0] <= {enable_pipe[PS-2:0],en};
    
-   assign enable = {enable_pipe[PS-1:0],en};
+   //Mask to 0 if no valid for last N cycles
+   assign enable = en | (|enable_pipe[N-1:0]);
 
-   assign dout[DW-1:0] =  {(DW){enable}} & din[DW-1:0];
+   assign dout[DW-1:0] = {(DW){enable}} & din[DW-1:0];
   
 endmodule // oh_datagate
