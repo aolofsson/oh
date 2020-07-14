126f8599081f354dafb9f7198dae83172bbb1a2c
Andreas.Olofsson <andreas@adapteva.com>
2020-07-14 13:48:42 -0400

    Adding clock enable for register -Removing the ASIC CFG as well...have to rethink that concept, not really working

--- common/hdl/oh_reg1.v
+++ common/hdl/oh_reg1.v
@@ -8,25 +8,19 @@
 module oh_reg1 #(parameter DW = 1            // data width
 		 ) 
    ( input           nreset, //async active low reset
-     input 	     clk, // clk, latch when clk=0
+     input 	     clk, // clk
+     input 	     en, // write enable
      input [DW-1:0]  in, // input data
      output [DW-1:0] out  // output data (stable/latched when clk=1)
      );
    
-`ifdef CFG_ASIC
-   asic_reg1 ireg [DW-1:0] (.nreset(nreset),
-			    .clk(clk),
-			    .in(in[DW-1:0]),
-			    .out(out[DW-1:0]));
-`else
    reg [DW-1:0]      out_reg;	   
    always @ (posedge clk or negedge nreset)
      if(!nreset)
        out_reg[DW-1:0] <= 'b0;
-     else	      
+     else if(en)	      
        out_reg[DW-1:0] <= in[DW-1:0];
    assign out[DW-1:0] = out_reg[DW-1:0];	   
-`endif // !`ifdef CFG_ASIC
    
 endmodule // ohr_reg1
 
