0d615202683f4c4b303d9391cf6e2a1a96faf58b
Andreas.Olofsson <andreas@adapteva.com>
2020-08-08 22:22:52 -0400

    Fixed issue with DC verilog parser -Apparentely "|=" is not allowed??

--- common/hdl/oh_mux.v
+++ common/hdl/oh_mux.v
@@ -21,7 +21,7 @@ module oh_mux #( parameter DW  = 1, // width of data inputs
      begin
 	out[DW-1:0] = 'b0;
 	for(i=0;i<N;i=i+1)
-	  out[DW-1:0] |= {(DW){sel[i]}} & in[((i+1)*DW-1)-:DW];
+	  out[DW-1:0] = out[DW-1:0] | {(DW){sel[i]}} & in[((i+1)*DW-1)-:DW];
      end
 
 endmodule // oh_mux
