fda0f35dd9da8494135383265bcc259aecd5bb8b
Andreas.Olofsson <andreas@adapteva.com>
2020-09-23 16:47:13 -0400

    Name change to packets -Using valid signal instead of access, more standardized

--- common/hdl/oh_fifo_cdc.v
+++ common/hdl/oh_fifo_cdc.v
@@ -10,24 +10,26 @@ module oh_fifo_cdc # (parameter DW        = 104,      //FIFO width
 		      parameter TARGET    = "GENERIC" //XILINX,ALTERA,GENERIC
 		      )
    (
-   input 	   nreset, // shared domain async active low reset
-   input 	   clk_in, // write clock
-   input 	   access_in, // write access
-   input [DW-1:0]  packet_in, // write packet
-   output 	   wait_out, // write pushback
-   input 	   clk_out, //read clock
-   output reg 	   access_out, //read access
-   output [DW-1:0] packet_out, //read packet
-   input 	   wait_in, // read pushback
-   output 	   prog_full, // fifo is half full
-   output 	   full, // fifo is full
-   output 	   empty // fifo is empty
+    input 	    nreset, // async active low reset
+    //Write Side
+    input 	    clk_in, // write clock
+    input 	    valid_in, // write valid
+    input [DW-1:0]  packet_in, // write packet
+    output 	    wait_out, // write pushback
+    //Read Side
+    input 	    clk_out, //read clock
+    output reg 	    valid_out, //read valid
+    output [DW-1:0] packet_out, //read packet
+    input 	    wait_in, // read pushback
+    //Status
+    output 	    prog_full, // fifo is half full
+    output 	    full, // fifo is full
+    output 	    empty // fifo is empty
     );
    
    // local wires
    wire 	   wr_en;
    wire 	   rd_en;
-   wire 	   io_nreset;
       
    // parametric async fifo
    oh_fifo_async  #(.TARGET(TARGET),
@@ -46,21 +48,22 @@ module oh_fifo_cdc # (parameter DW        = 104,      //FIFO width
 	 .rd_en	    (rd_en));
 
    // FIFO control logic
-   assign wr_en    = access_in;
+   assign wr_en    = valid_in;
    assign rd_en    = ~empty & ~wait_in;
-   assign wait_out = prog_full;         //wait_out should stall access_in signal
+   assign wait_out = prog_full;
 
-   // pipeline access_out signal
-   always @ (posedge clk_out or negedge io_nreset)
-     if(!io_nreset)
-       access_out <= 1'b0;   
-     else if(~wait_in)
-       access_out <= rd_en;
+
+   //async asser, sync deassert of reset
+   oh_rsync sync_reset(.nrst_out  (nreset_out),
+                       .clk       (clk_out),
+                       .nrst_in   (nreset));
    
-   // be safe, synchronize reset with clk_out
-   oh_rsync sync_reset(.nrst_out  (io_nreset),
-		       .clk	  (clk_out),
-		       .nrst_in	  (nreset));
+   //align valid signal with FIFO read delay
+   always @ (posedge clk_out or negedge nreset_out)
+     if(!nreset_out)
+       valid_out <= 1'b0;   
+     else if(~wait_in)
+       valid_out <= rd_en;
    
 endmodule // oh_fifo_cdc
 
