558776a3b3ff106360fde452bdb419d066d66dcd
Andreas.Olofsson <andreas@adapteva.com>
2020-10-09 13:18:03 -0400

    MILESTON: New packet format created for emesh

--- emesh/hdl/emesh2packet.v
+++ emesh/hdl/emesh2packet.v
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Function:  Memory Mapped Transaction to Packet Converter                                     
+ * Function:  Memory Mapped Transaction --> Packet Converter                                     
  * Author:    Andreas Olofsson                                                
  * License:   MIT (see LICENSE file in OH! repository)
  *
@@ -7,51 +7,95 @@
  * 
  * The following table shows the field mapping for different AW's:
  * 
- *  Packet    | AW16        | AW32     | AW64      | AW128    |
- *  --------------------------------------------------------
- *  [7:0]     | CMDL        | CMDL     | CMDL      | CMDL     |
- *  [39:8]    | D0/SA0,DA   | DA0      | DA0       | DA0      |
- *  [71:40]   | ****        | D0/SA0   | (D0/SA0)  | (D0/SA0) |
- *  [103:72]  | ****        | D1/SA1   | (D1/SA1)  | (D1/SA1) |
- *  [135:104] | ****        | ***,CMDH | DA1       | DA1      |
- *  [167:136] | ****        | ***,CMDH | D2/CMDH   | D2/SA2   |
- *  [199:168] | ***         | ****     | D3        | D3/SA3   |
- *  [231:200] | ****        | ***,CMDH | ***,CMDH  | DA2      |
- *  [263:232] | ****        | ****     | ****      | DA3      |
- *  [295:264] | ****        | ***,CMDH | ***,CMDH  | D4/CMDH  |
- *  [327:296] | ****        | ****     | ****      | D5       |
- *  [359:328] | ****        | ****     | ****      | D6       |
- *  [391:360] | ****        | ****     | ****      | D7       |
- *  [399:392] | ****        | ***,CMDH | ***,CMDH  | CMDH     |
+ *  | Packet  | AW16    | AW32     | AW64      | AW128   |
+ *  |---------|---------|----------|-----------|---------|
+ *  | 7:0     | CMDL    | CMDL     | CMDL      | CMDL    |
+ *  | 39:8    | D/SA,DA | DA0      | DA0       | DA0     |
+ *  | 71:40   | ****    | D0/SA1(0)| D0/SA1    | D0/SA1  |
+ *  | 103:72  | ****    | D1/SA0   | D1/SA0    | D1/SA0  |
+ *  | 135:104 | ****    | ***,CMDH | DA1       | DA1     |
+ *  | 167:136 | ****    | ***,CMDH | D2/CMDH   | D2/SA2  |
+ *  | 199:168 | ***     | ****     | D3        | D3/SA3  |
+ *  | 231:200 | ****    | ***,CMDH | ***,CMDH  | DA2     |
+ *  | 263:232 | ****    | ****     | ****      | DA3     |
+ *  | 295:264 | ****    | ***,CMDH | ***,CMDH  | D4/CMDH |
+ *  | 327:296 | ****    | ****     | ****      | D5      |
+ *  | 359:328 | ****    | ****     | ****      | D6      |
+ *  | 391:360 | ****    | ****     | ****      | D7      |
+ *  | 399:392 | ****    | ***,CMDH | ***,CMDH  | CMDH    |
  * 
  * The following list shows the widths supported for each AW
  * 
- *  Packet         | AW16  | AW32   | AW64   | AW128  |
- *  --------------------------------------------------
- *  minimum        | 40    | 72+8   | 136+8  | 264+8  |
- *  double/atomics | --    | 104+8  | 200+8  | 392+8  |
+ *  |Packet         | AW16  | AW32   | AW64   | AW128  |
+ *  |---------------|-------|--------|--------|--------|
+ *  |minimum        | 40    | 72+8   | 136+8  | 264+8  |
+ *  |double/atomics | --    | 104+8  | 200+8  | 392+8  |
  * 
- * This module is a pass through signal mappig module (no logic).
+ * The command field has the following options:
+ * 
+ * 
+ *  | Command[15:0]   | 15:8  | 7        | 6:4         | 3:0   |
+ *  |-----------------|-------|----------|-------------|-------|
+ *  | WRITE-SINGLE    | CH    | NEAR/FAR | SIZE[2:0]   | 0000  |
+ *  | WRITE-IRQ       | CH    | NEAR/FAR | SIZE[2:0]   | 0001  |
+ *  | WRITE-START     | CH    | NEAR/FAR | SIZE[2:0]   | 0010  |
+ *  | WRITE-STREAM    | CH    | NEAR/FAR | SIZE[2:0]   | 0011  |
+ *  | WRITE-STOP      | CH    | NEAR/FAR | SIZE[2:0]   | 0100  |
+ *  | WRITE-MULTICAST |       | NEAR/FAR | SIZE[2:0]   | 0101  |
+ *  | WRITE-IO        | CH    | NEAR/FAR | 64,DIR[1:0] | 0110  |
+ *  | TBD             | CH    | NEAR/FAR | SIZE[2:0]   | 0111  |
+ *  |-----------------|-------|----------|-------------|-------|
+ *  | READ            | CH    | NEAR/FAR | SIZE[2:0]   | 1000  |
+ *  | ATOMIC-OP       | CH    | NEAR/FAR | SIZE[2:0]   | 1001  |
+ *  | CAS             | CH    | NEAR/FAR | SIZE[2:0]   | 1010  |
+ *  | READ-IO         | CH    | NEAR/FAR | 64,DIR[1:0] | 1011  |
+ *  | TBD             | CH    | NEAR/FAR | SIZE[2:0]   | 1101  |
+ *  | TBD             | CH    | NEAR/FAR | SIZE[2:0]   | 1110  |
+ *  | TBD             | CH    | NEAR/FAR | SIZE[2:0]   | 1111  |
+ *  | TBD             | CH    | NEAR/FAR | SIZE[2:0]   | 1111  |
+ * 
+ * SIZE DECODE:
+ * 000=8b
+ * 001=16b
+ * 010=32b
+ * 011=64b
+ * 100=128b
+ * 1xx=reserved
+ * 
+ * DIR DECODE:
+ * 00=north
+ * 01=east
+ * 10=west
+ * 11=south
+ * 
  * Basic commands at bits [7:0] for all address widths
- * D0 is passed in the srcaddr inputs on write commands
+ * AW32/AW64/AW128 formats are compatible
+ * AW16 format is a standalone format not compatible with any other
+ * CMD[3] indicates
+ * Near/Far indicates whether to enable AW or AW/2 packet width
+ * All transactions are LSB aligned  
  * 
  ******************************************************************************/
-module emesh2packet #(parameter AW = 64,
-		      parameter PW = 144)
+module emesh2packet 
+  #(parameter AW = 64,
+    parameter PW = 144)
    (
     //Emesh signal bundle
-    input [15:0]    cmd_out,     //cmd[15:8] can be optionally set to zero
-    input [AW-1:0]  dstaddr_out, //destination address
-    input [AW-1:0]  srcaddr_out, //source address/lower data
-    input [AW-1:0]  data_out,    //upper data (optional)
+    input [15:0]     cmd_out, //cmd[15:8] can be optionally set to zero
+    input [AW-1:0]   dstaddr_out, //destination address
+    input [AW-1:0]   srcaddr_out, //source address (for reads)
+    input [2*AW-1:0] data_out, //data
     //Output packet
-    output [PW-1:0] packet_out
+    output [PW-1:0]  packet_out
     );
    
    //######################
    // PACKET COMMANDS
    //######################
-   assign packet_out[7:0]    = cmd_out[7:0];   
+   assign packet_out[7:0]  = cmd_out[7:0];
+
+   //Selector for src/data field
+   assign write  = cmd_out[3];
 
    generate
       //######################
@@ -60,7 +104,8 @@ module emesh2packet #(parameter AW = 64,
       if(AW==16) begin : aw16
 	 if(PW==40) begin : p40
 	    assign packet_out[23:8]  = dstaddr_out[15:0];
-	    assign packet_out[39:24] = srcaddr_out[15:0];
+	    assign packet_out[39:24] = write ? data_out[15:0]:
+				               srcaddr_out[15:0];
 	 end
 	 else begin: perror
 	    initial
@@ -73,22 +118,26 @@ module emesh2packet #(parameter AW = 64,
       if(AW==32) begin : aw32
 	 if(PW==72) begin: p72
 	    assign packet_out[39:8]   = dstaddr_out[31:0];
-	    assign packet_out[71:40]  = srcaddr_out[31:0];
+	    assign packet_out[71:40]  = write ? data_out[31:0] : 
+					        srcaddr_out[31:0];
 	 end
 	 else if(PW==80) begin: p80
 	    assign packet_out[39:8]   = dstaddr_out[31:0];
-	    assign packet_out[71:40]  = srcaddr_out[31:0];
+	    assign packet_out[71:40]  = write ? data_out[31:0] : 
+					        srcaddr_out[31:0];
 	    assign packet_out[79:72]  = cmd_out[15:8];
 	 end
 	 else if(PW==104) begin: p104
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = data_out[31:0];
+	    assign packet_out[71:40]   = data_out[31:0];	    
+	    assign packet_out[103:72]  = write ? data_out[63:32] :
+					         srcaddr_out[31:0];
 	 end
 	 else if(PW==112) begin: p112
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = data_out[31:0];
+	    assign packet_out[71:40]   = data_out[31:0];	    
+	    assign packet_out[103:72]  = write ? data_out[63:32] :
+					         srcaddr_out[31:0];
 	    assign packet_out[111:104] = cmd_out[15:8];
 	 end
 	 else begin: perror
@@ -102,30 +151,38 @@ module emesh2packet #(parameter AW = 64,
       if(AW==64) begin : aw64
 	 if(PW==136) begin: p136
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
 	 end
 	 else if(PW==144) begin: p144
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
 	    assign packet_out[143:136] = cmd_out[15:8];
 	 end
 	 else if(PW==200) begin: p200
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = data_out[63:0];
+	    assign packet_out[199:136] = data_out[127:64];
 	 end
 	 else if(PW==208) begin: p208
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = data_out[63:0];
+	    assign packet_out[199:136] = data_out[127:64];
 	    assign packet_out[207:200] = cmd_out[15:8];
 	 end
 	 else begin: perror
@@ -139,38 +196,46 @@ module emesh2packet #(parameter AW = 64,
       if(AW==128) begin : aw128
 	 if(PW==264) begin: p264
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = srcaddr_out[127:64];
+	    assign packet_out[199:136] = write ? data_out[127:64] :
+					         srcaddr_out[127:64];
 	    assign packet_out[263:200] = dstaddr_out[127:64];
 	 end
 	 else if(PW==272) begin: p272
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = srcaddr_out[127:64];
-	    assign packet_out[263:200] = dstaddr_out[127:64];
+	    assign packet_out[199:136] = write ? data_out[127:64] :
+					         srcaddr_out[127:64];
 	    assign packet_out[271:264] = cmd_out[15:8];
 	 end
 	 else if(PW==392) begin: p392
 	    assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = srcaddr_out[127:64];
-	    assign packet_out[263:200] = dstaddr_out[127:64];
-	    assign packet_out[391:264] = data_out[127:0];
+	    assign packet_out[199:136] = write ? data_out[127:64] :
+					         srcaddr_out[127:64];
+	    assign packet_out[391:264] = data_out[255:128];
 	 end
 	 else if(PW==400) begin: p400
-	     assign packet_out[39:8]    = dstaddr_out[31:0];
-	    assign packet_out[71:40]   = srcaddr_out[31:0];
-	    assign packet_out[103:72]  = srcaddr_out[63:32];
+	    assign packet_out[39:8]    = dstaddr_out[31:0];
+	    assign packet_out[71:40]   = write ? data_out[31:0] : 
+					         srcaddr_out[63:32];
+	    assign packet_out[103:72]  = write ? data_out[63:32] : 
+					         srcaddr_out[31:0];
 	    assign packet_out[135:104] = dstaddr_out[63:32];
-	    assign packet_out[199:136] = srcaddr_out[127:64];
-	    assign packet_out[263:200] = dstaddr_out[127:64];
-	    assign packet_out[391:264] = data_out[127:0];
+	    assign packet_out[199:136] = write ? data_out[127:64] :
+					         srcaddr_out[127:64];
 	    assign packet_out[399:392] = cmd_out[15:8];
 	 end
 	 else begin: perror
--- emesh/hdl/packet2emesh.v
+++ emesh/hdl/packet2emesh.v
@@ -1,92 +1,173 @@
-//#############################################################################
-//# Function: Maps Packet to Emesh Signals                                    #
-//#############################################################################
-//# Author:   Andreas Olofsson                                                #
-//# License:  MIT (see LICENSE file in OH! repository)                        # 
-//#############################################################################
-module packet2emesh #(parameter AW = 32,   // address width 
-		      parameter PW = 104)  // packet width
+/*******************************************************************************
+ * Function:  Packet-->Memory Mapped Transaction Converter                                     
+ * Author:    Andreas Olofsson                                                
+ * License:   MIT (see LICENSE file in OH! repository)
+ *
+ * Documentation:
+ * 
+ * see ./emesh2packet.v
+ * 
+ ******************************************************************************/
+module packet2emesh 
+  #(parameter AW = 32,   // address width 
+    parameter PW = 104)  // packet width
    (
     //Input packet
-    input [PW-1:0]  packet_in,
+    input [PW-1:0]    packet_in,
     //Emesh signal bundle 
-    output 	    write_in, // write signal
-    output [1:0]    datamode_in,// datasize
-    output [12:0]   ctrlmode_in,// ctrlmode
-    output [AW-1:0] dstaddr_in, // read/write address
-    output [AW-1:0] srcaddr_in, // return address for reads
-    output [AW-1:0] data_in // data
+    output [15:0]     cmd_in, // command
+    output [AW-1:0]   dstaddr_in, // read/write target address
+    output [AW-1:0]   srcaddr_in, // read return address
+    output [2*AW-1:0] data_in     // write data
     );
-      
-   // ---- FORMAT -----
-   //
-   // [0]  =write bit
-   // [2:1]=datamode
-   // [7:3]=ctrlmode
-   // [39:8]=dstaddr(lo)
-   //
-   // ---- 32-BIT ADDRESS ----
-   // [71:40]   data (lo)   | xxxx
-   // [103:72]  srcaddr(lo) | data (hi)
-   //
-   // ---- 64-BIT ADDRESS ----
-   // [71:40]   D0 | srcaddr(hi)
-   // [103:72]  D1 | srcaddr(lo)
-   // [135:104] dstaddr(hi)
+
+   //Command always situated in lowest byte
+   assign cmd_in[7:0] = packet_in[7:0];
    
    generate
-      if(PW==144)
-	begin : p144
-	   assign write_in           = packet_in[0];
-	   assign datamode_in[1:0]   = packet_in[2:1];  
-	   assign ctrlmode_in[12:0]  = {packet_in[143:136], packet_in[7:3]};
-	   assign dstaddr_in[63:0]   = {packet_in[135:104],packet_in[39:8]}; 
-	   assign srcaddr_in[63:0]   = {packet_in[71:40],packet_in[135:72]};
-	   assign data_in[63:0]      = packet_in[103:40];
-	end
-      else if(PW==136)
-	begin : p136
-	   assign write_in           = packet_in[0];
-	   assign datamode_in[1:0]   = packet_in[2:1];  
-	   assign ctrlmode_in[12:0]  = {8'b0, packet_in[7:3]};
-	   assign dstaddr_in[63:0]   = {packet_in[135:104],packet_in[39:8]}; 
-	   assign srcaddr_in[63:0]   = {packet_in[71:40],packet_in[135:72]};
-	   assign data_in[63:0]      = packet_in[103:40];
-	end
-      else if(PW==104)
-	begin : p104
-	   assign write_in           = packet_in[0];   
-	   assign datamode_in[1:0]   = packet_in[2:1];   
-	   assign ctrlmode_in[12:0]  = {9'b0,packet_in[6:3]};   
-	   assign dstaddr_in[31:0]   = packet_in[39:8]; 	 
-	   assign srcaddr_in[31:0]   = packet_in[103:72];  
-	   assign data_in[31:0]      = packet_in[71:40]; 
-	end
-      else if(PW==72)
-	begin : p72
-	   assign write_in           = packet_in[0];
-	   assign datamode_in[1:0]   = packet_in[2:1];  
-	   assign ctrlmode_in[12:0]  = {8'b0, packet_in[7:3]};
-	   assign dstaddr_in[31:0]   = packet_in[39:8]; 
-	   assign data_in[31:0]      = packet_in[71:40];
-	end
-      else if(PW==40)
-	begin : p40
-	   assign write_in           = packet_in[0];
-	   assign datamode_in[1:0]   = packet_in[2:1];  
-	   assign ctrlmode_in[12:0]  = {8'b0, packet_in[7:3]};
-	   assign dstaddr_in[15:0]   = packet_in[23:8]; 
-	   assign data_in[15:0]      = packet_in[39:24];
-	end
-      else
-	begin : unknown
-`ifdef TARGET_SIM
-	   initial
-	     $display ("packet width=%ds not supported",  PW);
-`endif
-	end
-   endgenerate
-   
+      //######################
+      // 16-Bit
+      //######################
+      if(AW==16) begin : aw16
+	 if(PW==40) begin : p40
+	    assign dstaddr_in[15:0] = packet_in[23:8];
+	    assign srcaddr_in[15:0] = packet_in[39:24];
+	    assign data_in[15:0]    = {16'b0,packet_in[39:24]};
+	 end
+	 else begin: perror
+	    initial
+	      $display ("Combo not supported (PW=%ds AW==%ds)", PW,AW);
+	 end
+      end // block: aw16
+      //######################
+      // 32-Bit
+      //######################
+      if(AW==32) begin : aw32
+	 if(PW==72) begin: p72
+	    assign dstaddr_in[31:0] = packet_in[39:8];
+	    assign srcaddr_in[31:0] = packet_in[71:40];
+	    assign data_in[63:0]    = {32'b0,packet_in[71:40]};
+	 end
+	 else if(PW==80) begin: p80
+	    assign dstaddr_in[31:0] = packet_in[39:8];
+	    assign srcaddr_in[31:0] = packet_in[71:40];
+	    assign data_in[63:0]    = {32'b0,packet_in[71:40]};
+	    assign cmd_in[15:8]     = packet_in[79:72];
+	 end
+	 else if(PW==104) begin: p104
+	    assign dstaddr_in[31:0] = packet_in[39:8];
+	    assign srcaddr_in[31:0] = packet_in[103:72];
+	    assign data_in[63:0]    = packet_in[103:40];
+	 end
+	 else if(PW==112) begin: p112
+	    assign dstaddr_in[31:0] = packet_in[39:8];
+	    assign srcaddr_in[31:0] = packet_in[103:72];
+	    assign data_in[63:0]    = packet_in[103:40];
+	    assign cmd_in[15:8]     = packet_in[111:104];
+	 end
+	 else begin: perror
+	    initial
+	      $display ("Combo not supported (PW=%ds AW==%ds)", PW,AW);
+	 end
+      end // block: aw32
+      //######################
+      // 64-Bit
+      //######################
+      if(AW==64) begin : aw64
+	 if(PW==136) begin: p136
+	    assign dstaddr_in[31:0]  = packet_in[39:8];
+	    assign dstaddr_in[63:32] = packet_in[135:104];
+	    assign srcaddr_in[31:0]  = packet_in[103:72];
+	    assign srcaddr_in[63:32] = packet_in[71:40];
+	    assign data_in[127:0]    = {64'b0,packet_in[103:40]};
+	 end
+	 else if(PW==144) begin: p144
+	    assign dstaddr_in[31:0]  = packet_in[39:8];
+	    assign dstaddr_in[63:32] = packet_in[135:104];
+	    assign srcaddr_in[31:0]  = packet_in[103:72];
+	    assign srcaddr_in[63:32] = packet_in[71:40];
+	    assign data_in[127:0]    = {64'b0,packet_in[103:40]};
+	    assign cmd_in[15:8]      = packet_in[143:136];
+	 end
+	 else if(PW==200) begin: p200
+	    assign dstaddr_in[31:0]  = packet_in[39:8];
+	    assign dstaddr_in[63:32] = packet_in[135:104];
+	    assign srcaddr_in[31:0]  = packet_in[103:72];
+	    assign srcaddr_in[63:32] = packet_in[71:40];
+	    assign data_in[63:0]     = packet_in[103:40];
+	    assign data_in[127:64]   = packet_in[199:136];
+	 end
+	 else if(PW==208) begin: p208
+	    assign dstaddr_in[31:0]  = packet_in[39:8];
+	    assign dstaddr_in[63:32] = packet_in[135:104];
+	    assign srcaddr_in[31:0]  = packet_in[103:72];
+	    assign srcaddr_in[63:32] = packet_in[71:40];
+	    assign data_in[63:0]     = packet_in[103:40];
+	    assign data_in[127:64]   = packet_in[199:136];
+	    assign cmd_in[15:8]      = packet_in[207:200];
+	 end
+	 else begin: perror
+	    initial
+	      $display ("Combo not supported (PW=%ds AW==%ds)", PW,AW);
+	 end
+      end // block: aw64
+      //######################
+      // 128-Bit
+      //######################
+      if(AW==128) begin : aw128
+	 if(PW==264) begin: p264
+	    assign dstaddr_in[31:0]   = packet_in[39:8];
+	    assign dstaddr_in[63:32]  = packet_in[135:104];
+	    assign dstaddr_in[127:64] = packet_in[263:200];
+	    assign srcaddr_in[31:0]   = packet_in[103:72];
+	    assign srcaddr_in[63:32]  = packet_in[71:40];
+	    assign srcaddr_in[127:64] = packet_in[199:136];
+	    assign data_in[63:0]      = packet_in[103:40];
+	    assign data_in[127:64]    = packet_in[199:136];
+	    assign data_in[255:128]   = 128'b0;
+	 end
+	 else if(PW==272) begin: p272
+	    assign dstaddr_in[31:0]   = packet_in[39:8];
+	    assign dstaddr_in[63:32]  = packet_in[135:104];
+	    assign dstaddr_in[127:64] = packet_in[263:200];
+	    assign srcaddr_in[31:0]   = packet_in[103:72];
+	    assign srcaddr_in[63:32]  = packet_in[71:40];
+	    assign srcaddr_in[127:64] = packet_in[199:136];
+	    assign data_in[63:0]      = packet_in[103:40];
+	    assign data_in[127:64]    = packet_in[199:136];
+	    assign data_in[255:128]   = 128'b0;
+	    assign cmd_in[15:8]       = packet_in[271:264];
+	 end
+	 else if(PW==392) begin: p392
+	    assign dstaddr_in[31:0]   = packet_in[39:8];
+	    assign dstaddr_in[63:32]  = packet_in[135:104];
+	    assign dstaddr_in[127:64] = packet_in[263:200];
+	    assign srcaddr_in[31:0]   = packet_in[103:72];
+	    assign srcaddr_in[63:32]  = packet_in[71:40];
+	    assign srcaddr_in[127:64] = packet_in[199:136];
+	    assign data_in[63:0]      = packet_in[103:40];
+	    assign data_in[127:64]    = packet_in[199:136];
+	    assign data_in[255:128]   = packet_in[391:264];
+	 end
+	 else if(PW==400) begin: p400
+	    assign dstaddr_in[31:0]   = packet_in[39:8];
+	    assign dstaddr_in[63:32]  = packet_in[135:104];
+	    assign dstaddr_in[127:64] = packet_in[263:200];
+	    assign srcaddr_in[31:0]   = packet_in[103:72];
+	    assign srcaddr_in[63:32]  = packet_in[71:40];
+	    assign srcaddr_in[127:64] = packet_in[199:136];
+	    assign data_in[63:0]      = packet_in[103:40];
+	    assign data_in[127:64]    = packet_in[199:136];
+	    assign data_in[255:128]   = packet_in[391:264];
+	    assign cmd_in[15:8]       = packet_in[399:392];
+	 end
+	 else begin: perror
+	    initial
+	      $display ("Combo not supported (PW=%ds AW==%ds)", PW,AW);
+	 end
+      end // block: aw128
+   endgenerate  
+
 endmodule // packet2emesh
 
 
