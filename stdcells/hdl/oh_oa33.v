5e151efa511856595eab1ee427ac06599797cada
aolofsson <andreas@zeroasic.com>
2021-05-24 20:26:05 -0400

    Adding batch of oa/ao cells

---
+++ stdcells/hdl/oh_oa33.v
@@ -0,0 +1,20 @@
+//#############################################################################
+//# Function: Or-And (oa33) Gate                                              #
+//# Copyright: OH Project Authors. ALl rights Reserved.                       #
+//# License:  MIT (see LICENSE file in OH repository)                         # 
+//#############################################################################
+
+module oh_oa33 #(parameter DW = 1 ) // array width
+   (
+    input [DW-1:0]  a0,
+    input [DW-1:0]  a1,
+    input [DW-1:0]  a2,
+    input [DW-1:0]  b0,
+    input [DW-1:0]  b1,
+    input [DW-1:0]  b2, 
+    output [DW-1:0] z
+    );
+   
+   assign z = (a0 | a1 | a2) & (b0 | b1 | b2);
+   
+endmodule
---
+++ stdcells/hdl/oh_oai21.v
@@ -0,0 +1,17 @@
+//#############################################################################
+//# Function: Or-And-Inverter (oai21) Gate                                    #
+//# Copyright: OH Project Authors. ALl rights Reserved.                       #
+//# License:  MIT (see LICENSE file in OH repository)                         # 
+//#############################################################################
+
+module oh_oai21 #(parameter DW = 1 ) // array width
+   (
+    input [DW-1:0]  a0,
+    input [DW-1:0]  a1,
+    input [DW-1:0]  b0, 
+    output [DW-1:0] z
+    );
+   
+   assign z = ~((a0 | a1) & b0);
+   
+endmodule
