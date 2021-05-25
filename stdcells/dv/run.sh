2eb3e4518b88139f80d9b348118522aeceb0a20f
aolofsson <andreas@zeroasic.com>
2021-05-25 16:10:50 -0400

    Prototype for driving cell level parameters

--- stdcells/dv/run.sh
+++ stdcells/dv/run.sh
@@ -1,12 +1,12 @@
 #NAND2
-sv2v  oh_nand2_tb.sv > oh_nand2_tb.v
-sv2v  ../netlist/oh_nand2.sv > ../netlist/oh_nand2.v
-iverilog oh_nand2_tb.v ../netlist/oh_nand2.v ../netlist/oh_nmos.v ../netlist/oh_pmos.v -o nand2.out
+sv2v  oh_nand2_tb.sv > oh_nand2_tb.v.tmp
+sv2v  ../netlist/oh_nand2.sv > ../netlist/oh_nand2.v.tmp
+iverilog oh_nand2_tb.v.tmp ../netlist/oh_nand2.v.tmp ../netlist/oh_nmos.sv ../netlist/oh_pmos.sv -o nand2.out
 
 #NOR2
-sv2v  oh_nor2_tb.sv > oh_nor2_tb.v
-sv2v  ../netlist/oh_nor2.sv > ../netlist/oh_nor2.v
-iverilog oh_nor2_tb.v ../netlist/oh_nor2.v ../netlist/oh_nmos.v ../netlist/oh_pmos.v -o nor2.out
+sv2v  oh_nor2_tb.sv > oh_nor2_tb.v.tmp
+sv2v  ../netlist/oh_nor2.sv > ../netlist/oh_nor2.v.tmp
+iverilog oh_nor2_tb.v.tmp ../netlist/oh_nor2.v.tmp ../netlist/oh_nmos.sv ../netlist/oh_pmos.sv -o nor2.out
 
 
 
