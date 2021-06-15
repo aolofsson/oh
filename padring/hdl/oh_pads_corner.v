403976b8c94ce4300d452fde7b0593065575e9cd
aolofsson <andreas@zeroasic.com>
2021-06-15 11:48:39 -0400

    Adding corner pad place holder

---
+++ padring/hdl/oh_pads_corner.v
@@ -0,0 +1,16 @@
+//#############################################################################
+//# Function: Corner Pads                                                     #
+//# Copyright: OH Project Authors. ALl rights Reserved.                       #
+//# License:  MIT (see LICENSE file in OH repository)                         # 
+//#############################################################################
+
+module oh_pads_corner
+  (//feed through signals
+   inout 	       vddio, // io supply
+   inout 	       vssio, // io ground
+   inout 	       vdd, // core supply
+   inout 	       vss // common ground
+   );
+  
+endmodule // oh_pads_corner
+
