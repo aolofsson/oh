421fcf6340bec8074dc6c8383798702537c8eefa
aolofsson <andreas@zeroasic.com>
2021-07-26 11:49:10 -0400

    Implementign asic cell redirection for clock gates

--- common/hdl/oh_clockgate.v
+++ common/hdl/oh_clockgate.v
@@ -2,34 +2,45 @@
 //# Function: Low power clock gate circuit                                    #
 //#############################################################################
 //# Author:   Andreas Olofsson                                                #
-//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//# License:  MIT (see LICENSE file in OH! repository)                        #
 //#############################################################################
 
-module oh_clockgate (
-     input  clk, // clock input 
-     input  te, // test enable   
-     input  en, // enable (from positive edge FF)
-     output eclk // enabled clock output
-     );
+module oh_clockgate
+  #(parameter SYN  = "TRUE",    // synthesizable
+    parameter TYPE = "DEFAULT"  // implementation type
+    )
+(
+ input 	clk, // clock input
+ input 	te,  // test enable
+ input 	en,  // enable (from positive edge FF)
+ output eclk // enabled clock output
+ );
 
-`ifdef CFG_ASIC
-   asic_icg icg (.en(en),
-		 .te(te),
-		 .clk(clk),
-		 .eclk(eclk));
-`else
-   wire     en_sh;
-   wire     en_sl;
-   //Stable low/valid rising edge enable
-   assign   en_sl = en | te;
-   //Stable high enable signal
-   oh_lat0 lat0 (.out (en_sh),
-		 .in  (en_sl),
-		 .clk (clk));
-   
-   assign eclk =  clk & en_sh;
-`endif // !`ifdef CFG_ASIC
-        
-endmodule // oh_clockgate
+   generate
+      if(SYN == "TRUE") begin
 
+	 wire     en_sh;
+	 wire     en_sl;
 
+	 //Stable low/valid rising edge enable
+	 assign   en_sl = en | te;
+
+	 //Stable high enable signal
+	 oh_lat0 lat0 (.out (en_sh),
+		       .in  (en_sl),
+		       .clk (clk));
+
+	 assign eclk =  clk & en_sh;
+
+      end
+      else begin
+	 asic_clockgate #(.TYPE(TYPE))
+	 asic_clockgate (// Outputs
+			 .eclk		(eclk),
+			 // Inputs
+			 .clk		(clk),
+			 .te		(te),
+			 .en		(en));
+      end
+   endgenerate
+endmodule
--- common/hdl/oh_clockmux.v
+++ common/hdl/oh_clockmux.v
@@ -2,35 +2,32 @@
 //# Function: Clock mux                                                       #
 //#############################################################################
 //# Author:   Andreas Olofsson                                                #
-//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//# License:  MIT (see LICENSE file in OH! repository)                        #
 //#############################################################################
 
-module oh_clockmux #(parameter N    = 1)    // number of clock inputs
+module oh_clockmux
+  #(parameter N    = 2,        // number of clock inputs)
+    parameter SYN  = "TRUE",   // synthesizable (or not)
+    parameter TYPE = "DEFAULT" // implementation type
+    )
    (
-    input [N-1:0] en, // one hot enable vector (needs to be stable!)
-    input [N-1:0] clkin,// one hot clock inputs (only one is active!) 
-    output 	  clkout 
+    input [N-1:0] en,    // one hot enable vector (needs to be stable!)
+    input [N-1:0] clkin, // one hot clock inputs (only one is active!)
+    output 	  clkout // clock output
     );
 
-`ifdef CFG_ASIC
-    generate
-       if((N==2))
-	 begin : asic
-	    asic_clockmux2 imux (.clkin(clkin[N-1:0]),
-				 .en(en[N-1:0]),
-				 .clkout(clkout));
-	 end
-       else if((N==4))
-	 begin : asic
-	    asic_clockmux4 imux (.clkin(clkin[N-1:0]),
-				 .en(en[N-1:0]),
-				 .clkout(clkout));
-	 end
-    endgenerate
-`else // !`ifdef CFG_ASIC
-       assign clkout = |(clkin[N-1:0] & en[N-1:0]);
-`endif
-       
-endmodule // oh_clockmux
-
-
+   generate
+      if(SYN == "TRUE") begin
+	 assign clkout = |(clkin[N-1:0] & en[N-1:0]);
+      end
+      else begin
+	 asic_clockmux #(.TYPE(TYPE),
+			 .N(N))
+	 asic_clockmux(// Outputs
+		       .clkout		(clkout),
+		       // Inputs
+		       .en		(en[N-1:0]),
+		       .clkin		(clkin[N-1:0]));
+      end
+   endgenerate
+endmodule
