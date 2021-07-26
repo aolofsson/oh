9e8263b17da570fbf6c0c6662cc45d8fb169da3b
aolofsson <andreas@zeroasic.com>
2021-07-26 11:30:29 -0400

    WIP: Making delay programmable based on selector -More generatlized than a statically compiled parameter -Synthesis engine should optimize away redundant logic

--- common/hdl/oh_delay.v
+++ common/hdl/oh_delay.v
@@ -2,33 +2,35 @@
 //# Function: Delays input signal by N clock cycles                           #
 //#############################################################################
 //# Author:   Andreas Olofsson                                                #
-//# License:  MIT (see LICENSE file in OH! repository)                        # 
+//# License:  MIT (see LICENSE file in OH! repository)                        #
 //#############################################################################
 
-module oh_delay  #(parameter DW = 1, // width of data
-		   parameter N  = 1  // clock cycle delay by
-		   )   
+module oh_delay
+  #(parameter N        = 1,               // width of data
+    parameter MAXDELAY = 4,               // maximum delay cycles
+    parameter M        = $clog2(MAXDELAY) // delay selctor
+    )
    (
-    input [DW-1:0]  in, // input
-    input 	    clk,//clock input
-    output [DW-1:0] out // output
+    input 	   clk, //clock input
+    input [N-1:0]  in, // input vector
+    input [M-1:0]  sel, // delay selector
+    output [N-1:0] out  // output vector
     );
 
-   reg [DW-1:0]     sync_pipe[N-1:0];
-      
+
+   //Delay pipeline
+   reg [N-1:0]     sync_pipe[MAXDELAY-1:0];
    genvar 	    i;
    generate
       always @ (posedge clk)
-	sync_pipe[0]<=in[DW-1:0];
-      for(i=1;i<N;i=i+1) begin: gen_pipe
+	sync_pipe[0]<=in[N-1:0];
+      for(i=1;i<MAXDELAY;i=i+1) begin: gen_pipe
          always @ (posedge clk)
 	   sync_pipe[i]<=sync_pipe[i-1];
       end
    endgenerate
 
-   assign out[DW-1:0] = sync_pipe[N-1];
-   
-endmodule // oh_delay
+   //Delay selector
+   assign out[N-1:0] = sync_pipe[sel[M-1:0]];
 
-
-
+endmodule
