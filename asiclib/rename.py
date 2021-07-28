df731e4e13586ce9ece0bf50fa5a298decdfa3de
aolofsson <andreas@zeroasic.com>
2021-07-28 08:48:15 -0400

    Adding asiclib scripts directory

--- asiclib/rename.py
+++
@@ -1,8 +0,0 @@
-import glob
-import re
-import os
-file_list = glob.glob('*.v')
-for item in file_list:
-    newfile = item.replace("oh_", "asic_")
-    print (item, newfile)
-    os.rename(item, newfile)
